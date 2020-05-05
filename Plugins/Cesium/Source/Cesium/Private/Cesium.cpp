6b6e762736d94bb4daa7e2acc618202e5e0912ed
Kevin Ring <kevin@kotachrome.com>
2020-05-05 15:04:49 +1000

    Initial version of blank project and Cesium plugin.

---
+++ Plugins/Cesium/Source/Cesium/Private/Cesium.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Cesium.h"
+
+#define LOCTEXT_NAMESPACE "FCesiumModule"
+
+void FCesiumModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FCesiumModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCesiumModule, Cesium)
---
+++ Source/cesiumunreal/cesiumunrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "cesiumunrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CESIUMUNREAL_API AcesiumunrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
