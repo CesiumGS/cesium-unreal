4092b70546fe63828fdcbc93dd4c420015a00073
Kevin Ring <kevin@kotachrome.com>
2020-05-07 20:48:48 +1000

    Basic glTF working.

--- Plugins/Cesium/Source/Cesium/Private/tinygltf.cpp
+++ Plugins/Cesium/Source/Cesium/Private/tinygltf.cpp
@@ -4,4 +4,5 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 // #define TINYGLTF_NOEXCEPTION // optional. disable exception handling.
 #define TINYGLTF_LITTLE_ENDIAN
+#define TINYGLTF_ENABLE_DRACO
 #include "tiny_gltf.h"
