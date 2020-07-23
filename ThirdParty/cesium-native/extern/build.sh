983238fa10fbda5aac13bbb16d23b2593380fe96
Kevin Ring <kevin@kotachrome.com>
2020-07-23 16:30:56 +1000

    Run tests on Travis.

--- ThirdParty/cesium-native/extern/build.sh
+++ ThirdParty/cesium-native/extern/build.sh
@@ -5,13 +5,13 @@ SCRIPT=$(readlink -f "$0")
 EXTERNDIR=$(dirname "$SCRIPT")
 echo $EXTERNDIR
 cd $EXTERNDIR
-mkdir build
+mkdir -p build
 cd build
 
 # Draco
 cd $EXTERNDIR
 cd build
-mkdir draco
+mkdir -p draco
 cd draco
 cmake ../../draco/
 cmake --build . --config Release
@@ -19,7 +19,7 @@ cmake --build . --config Release
 # uriparser
 cd $EXTERNDIR
 cd build
-mkdir uriparser
+mkdir -p uriparser
 cd uriparser
 cmake ../../uriparser/ -DCMAKE_BUILD_TYPE=Release -DURIPARSER_BUILD_TESTS:BOOL=OFF -DURIPARSER_BUILD_DOCS:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=OFF -DURIPARSER_ENABLE_INSTALL:BOOL=OFF -DURIPARSER_BUILD_TOOLS:BOOL=OFF
 cmake --build . --config Release
