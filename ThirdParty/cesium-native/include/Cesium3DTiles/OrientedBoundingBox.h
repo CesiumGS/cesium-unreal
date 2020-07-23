6bf30eaa3758701e2e8ed0c1121dfc64920f85fd
Kevin Ring <kevin@kotachrome.com>
2020-07-23 13:44:14 +1000

    Add Doxygen config, add some doc, rename TestBoundingBox.cpp.

--- ThirdParty/cesium-native/include/Cesium3DTiles/OrientedBoundingBox.h
+++ ThirdParty/cesium-native/include/Cesium3DTiles/OrientedBoundingBox.h
@@ -10,8 +10,24 @@ namespace Cesium3DTiles {
 
     class Plane;
 
+    /**
+     * @brief A {@link BoundingVolume} defined as a closed and convex cuboid with any orientation.
+     * 
+     * @see BoundingSphere
+     * @see BoundingRegion
+     */
     class OrientedBoundingBox {
     public:
+        /**
+         * Computes an {@link OrientedBoundingBox} that bounds a {@link Rectangle} near the surface
+         * of an {@link Ellipsoid}. There are no guarantees about the orientation of the bounding box.
+         * 
+         * @param rectangle The cartographic rectangle on the surface of the ellipsoid.
+         * @param minimumHeight The minimum height (elevation) within the rectangle, in meters above the ellipsoid surface.
+         * @param maximumHeight The maximum height (elevation) within the rectangle, in meters above the ellipsoid surface.
+         * @param ellipsoid The ellipsoid on which the rectangle is defined.
+         * @return The computed bounding box.
+         */
         static OrientedBoundingBox fromRectangle(
             const Rectangle& rectangle,
             double minimumHeight = 0.0,
@@ -19,41 +35,55 @@ namespace Cesium3DTiles {
             const Ellipsoid& ellipsoid = Ellipsoid::WGS84
         );
 
+        /**
+         * @brief Constructs a new instance.
+         * 
+         * @param center The center of the box.
+         * @param halfAxes The three orthogonal half-axes of the bounding box. Equivalently, the
+         *                 transformation matrix to rotate and scale a 0x0x0 cube centered at the
+         *                 origin.
+         * 
+         * @snippet TestOrientedBoundingBox.cpp Constructor
+         */
         OrientedBoundingBox(
             const glm::dvec3& center,
             const glm::dmat3& halfAxes
         ) :
             _center(center),
             _halfAxes(halfAxes)
-        {}
+        {
+        }
 
         /**
-         * @brief Get the Center object
-         * 
-         * @return const glm::dvec3& 
+         * @brief Gets the center of the box.
          */
         const glm::dvec3& getCenter() const { return this->_center; }
+
+        /**
+         * @brief Gets the transformation matrix, to rotate and scale the box to the right position and size.
+         */
         const glm::dmat3& getHalfAxes() const { return this->_halfAxes; }
 
-        /// <summary>
-        /// Determines on which side of a plane the bounding box is located.
-        /// </summary>
-        /// <param name="plane">The plane to test.</param>
-        /// <returns>
-        /// <see cref="CullingResult::Inside"/> if the entire box is on the side of the plane
-        /// the normal is pointing, <see cref="CullingResult::Outside"/> if the entire box is
-        /// on the opposite side, and <see cref="CullingResult::Intersecting"/> if the box
-        /// intersects the plane.
-        /// </returns>
+        /**
+         * @brief Determines on which side of a plane the bounding box is located.
+         * 
+         * @param plane The plane to test against.
+         * @return
+         *  * {@link CullingResult::Inside} if the entire box is on the side of the plane the normal is pointing.
+         *  * {@link CullingResult::Outside} if the entire box is on the opposite side.
+         *  * {@link CullingResult::Intersecting} if the box intersects the plane.
+         */
         CullingResult intersectPlane(const Plane& plane) const;
 
-        /// <summary>
-        /// Computes the distance squared from a given position to the closest
-        /// point on this bounding volume. The bounding volume and the position
-        /// must be expressed in the same coordinate system.
-        /// </summary>
-        /// <param name="position">The position.</param>
-        /// <returns>The squared distance.</returns>
+        /**
+         * @brief Computes the distance squared from a given position to the closest point on this bounding volume.
+         * The bounding volume and the position must be expressed in the same coordinate system.
+         * 
+         * @param position The position
+         * @return The estimated distance squared from the bounding box to the point.
+         * 
+         * @snippet TestOrientedBoundingBox.cpp distanceSquaredTo
+         */
         double computeDistanceSquaredToPosition(const glm::dvec3& position) const;
 
     private:
