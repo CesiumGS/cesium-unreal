ca4693bd9ab87491d4b5b2688b1fde3c8518dd23
Kevin Ring <kevin@kotachrome.com>
2020-11-06 15:53:53 +1100

    Don't tick UCesiumGltfPrimitiveComponent unnecessarily.

--- Source/Cesium/Private/UCesiumGltfPrimitiveComponent.cpp
+++ Source/Cesium/Private/UCesiumGltfPrimitiveComponent.cpp
@@ -8,7 +8,7 @@ UCesiumGltfPrimitiveComponent::UCesiumGltfPrimitiveComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
