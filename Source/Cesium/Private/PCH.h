646f94cc7b881ae552c0ac1a41876024be002e88
Kevin Ring <kevin@kotachrome.com>
2021-03-04 15:20:26 +1100

    Fix remaining build problems under VS2017.

--- Source/Cesium/Private/PCH.h
+++ Source/Cesium/Private/PCH.h
@@ -4,3 +4,15 @@
 
 #define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 #include "CoreMinimal.h"
+
+// Workaround for an unhelpful warning (that gets treated as an error) in VS2017.
+// See https://github.com/akrzemi1/Optional/issues/57 and
+// https://answers.unrealengine.com/questions/607946/anonymous-union-with-none-trivial-type.html
+#if _MSC_VER < 1920
+#pragma warning(push)
+#pragma warning(disable:4583)
+#pragma warning(disable:4582)
+#include <optional>
+#include <variant>
+#pragma warning(pop)
+#endif
