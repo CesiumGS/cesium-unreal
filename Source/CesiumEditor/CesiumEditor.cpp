8e4c0dd0e02ffa99dc34d792efddfdb804ad86dc
Kevin Ring <kevin@kotachrome.com>
2021-03-08 19:06:04 +1100

    Resuming ion sessions next time editor is launched.

--- Source/CesiumEditor/CesiumEditor.cpp
+++ Source/CesiumEditor/CesiumEditor.cpp
@@ -43,6 +43,7 @@ void FCesiumEditorModule::StartupModule()
     CesiumAsync::AsyncSystem asyncSystem(std::make_shared<UnrealTaskProcessor>());
 
     this->_pIonSession = std::make_shared<CesiumIonSession>(asyncSystem, pAssetAccessor);
+    this->_pIonSession->resume();
 
     // Only register style once
     if (!StyleSet.IsValid())
