c0112ba5e81d75df1a6b9f6ec95bd548bcd82d78
Bao Tran <tranthienbao123@gmail.com>
2021-04-13 13:46:49 -0400

    add space in bash

--- travis/travis-get-ue.sh
+++ travis/travis-get-ue.sh
@@ -6,7 +6,7 @@ then
     AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/2021-03-16/Launcher.zip .
     7z x Launcher.zip "-oC:\Program Files (x86)\Epic Games"
     rm Launcher.zip
-elif [[ $TRAVIS_OS_NAME == "osx"]]
+elif [[ $TRAVIS_OS_NAME == "osx" ]]
 then
     gsutil cp gs://cesium-unreal-engine/macos-2021-04-13/UE_4.26.tar.gz $HOME/UE_4.26.tar.gz
     tar xfz UE_4.26.tar.gz -C $HOME
