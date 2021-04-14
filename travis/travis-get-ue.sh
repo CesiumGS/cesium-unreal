c3c2aa5b21d66b27b4f578c434bf8e426d60bfec
Bao Tran <tranthienbao123@gmail.com>
2021-04-13 23:07:01 -0400

    fix directory that is used to extract UE engine

--- travis/travis-get-ue.sh
+++ travis/travis-get-ue.sh
@@ -9,6 +9,7 @@ then
 elif [[ $TRAVIS_OS_NAME == "osx" ]]
 then
     AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/macos-2021-04-13/UE_4.26.tar.gz .
-    tar xfz UE_4.26.tar.gz -C $HOME
+    mkdir $HOME/UE_4.26
+    tar xfz UE_4.26.tar.gz -C $HOME/UE_4.26
     rm UE_4.26.tar.gz
 fi
