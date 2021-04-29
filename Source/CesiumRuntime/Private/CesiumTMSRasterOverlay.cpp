7c470ce33dd1aa6db530ee03b2651582b5e2fb37
Aiden <aidensoedjarwo@gmail.com>
2021-04-29 18:10:53 +1000

    Fix for GWC: Add min/max zoom levels.

---
+++ Source/CesiumRuntime/Private/CesiumTMSRasterOverlay.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright 2020-2021 CesiumGS, Inc. and Contributors
+
+
+#include "CesiumTMSRasterOverlay.h"
+
+#include "CesiumRuntime.h"
+#include <Cesium3DTiles/TileMapServiceRasterOverlay.h>
+
+std::unique_ptr<Cesium3DTiles::RasterOverlay>
+UCesiumTMSRasterOverlay::CreateOverlay() {
+  Cesium3DTiles::TileMapServiceRasterOverlayOptions Options;
+  if (MaximumLevel > MinimumLevel) {
+    Options.minimumLevel = MinimumLevel;
+    Options.maximumLevel = MaximumLevel;
+  }
+  return std::make_unique<Cesium3DTiles::TileMapServiceRasterOverlay>(
+    TCHAR_TO_UTF8(*this->SourceURL),
+    std::vector<CesiumAsync::IAssetAccessor::THeader>(),
+    Options);
+}
