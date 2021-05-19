846353aa9004939c6bd95eb119627653a761303b
Shehzan Mohammed <mzshehzanayub@gmail.com>
2021-05-19 16:16:46 -0400

    Add message output to travis-get-android-ndk script

--- travis/travis-get-android-ndk.sh
+++ travis/travis-get-android-ndk.sh
@@ -1,7 +1,13 @@
-if [[ $TRAVIS_OS_NAME == "windows" && ! -d "$HOME/android-ndk-r21e" ]]
+if [[ $TRAVIS_OS_NAME == "windows" ]]
 then
-    wget -q https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip
-    7z x android-ndk-r21e-windows-x86_64.zip "-o$HOME/"
-    rm android-ndk-r21e-windows-x86_64.zip
+    echo "Setting up Android NDK"
+    if [[ ! -d "$HOME/android-ndk-r21e" ]]
+    then
+        echo "Downloading Android NDK"
+        wget -q https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip
+        7z x android-ndk-r21e-windows-x86_64.zip "-o$HOME/"
+        rm android-ndk-r21e-windows-x86_64.zip
+    else
+        echo "Using Android NDK from Travis Cache"
+    fi
 fi
-
