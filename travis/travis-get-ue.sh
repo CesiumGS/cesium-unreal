d71db62a6570589193cbcd2b12159189306d6d4b
Kevin Ring <kevin@kotachrome.com>
2021-05-19 17:43:08 +1000

    Use new UE zip.
    This one doesn't include Linux support, and doesn't include the Engine
    source.

--- travis/travis-get-ue.sh
+++ travis/travis-get-ue.sh
@@ -5,7 +5,7 @@ then
     compact //c "//s:C:\Program Files\Epic Games"
     mkdir "C:\Program Files (x86)\Epic Games"
     compact //c "//s:C:\Program Files (x86)\Epic Games"
-    AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/windows-2021-05-07/UE_4.26.zip .
+    AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/windows-2021-05-19/UE_4.26.zip .
     7z x UE_4.26.zip "-oC:\Program Files\Epic Games"
     rm UE_4.26.zip
     AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/windows-2021-05-07/Launcher.zip .
