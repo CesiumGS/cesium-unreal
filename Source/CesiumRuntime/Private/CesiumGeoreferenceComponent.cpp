ef7dd81334650b071a340e283ea6be96f6d37d1c
Marco Hutter <javagl@javagl.de>
2021-05-29 16:46:52 +0200

    Don't assume default ellipsoid here either

--- Source/CesiumRuntime/Private/CesiumGeoreferenceComponent.cpp
+++ Source/CesiumRuntime/Private/CesiumGeoreferenceComponent.cpp
@@ -65,7 +65,7 @@ void UCesiumGeoreferenceComponent::SnapLocalUpToEllipsoidNormal() {
 
 void UCesiumGeoreferenceComponent::SnapToEastSouthUp() {
   glm::dmat4 ENUtoECEF = CesiumGeospatial::Transforms::eastNorthUpToFixedFrame(
-      this->_actorToECEF[3]);
+      this->_actorToECEF[3], CesiumGeospatial::Ellipsoid::WGS84);
   this->_actorToECEF = ENUtoECEF * CesiumTransforms::scaleToCesium *
                        CesiumTransforms::unrealToOrFromCesium;
   ;
@@ -424,9 +424,9 @@ void UCesiumGeoreferenceComponent::_setECEF(
     // the poles
     glm::dmat4 startEcefToEnu = glm::affineInverse(
         CesiumGeospatial::Transforms::eastNorthUpToFixedFrame(
-            this->_actorToECEF[3]));
+            this->_actorToECEF[3], CesiumGeospatial::Ellipsoid::WGS84));
     glm::dmat4 endEnuToEcef =
-        CesiumGeospatial::Transforms::eastNorthUpToFixedFrame(targetEcef);
+        CesiumGeospatial::Transforms::eastNorthUpToFixedFrame(targetEcef, CesiumGeospatial::Ellipsoid::WGS84);
     this->_actorToECEF = endEnuToEcef * startEcefToEnu * this->_actorToECEF;
   }
 
