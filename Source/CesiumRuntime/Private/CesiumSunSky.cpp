f29573b8fd7c16ebd25f12e96ab9525745beba29
Xuelong Mu <xuelongmu@gmail.com>
2021-06-18 18:09:26 -0400

    Brought over all properties to C++ and some helper methods, as well as georeference update handling. "gameplay" logic remains in BP

--- Source/CesiumRuntime/Private/CesiumSunSky.cpp
+++ Source/CesiumRuntime/Private/CesiumSunSky.cpp
@@ -2,10 +2,53 @@
 
 #include "CesiumSunSky.h"
 #include "CesiumRuntime.h"
+#include "UObject/ConstructorHelpers.h"
 
 // Sets default values
 ACesiumSunSky::ACesiumSunSky() {
-      PrimaryActorTick.bCanEverTick = false;
+  PrimaryActorTick.bCanEverTick = false;
+
+  Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+  SetRootComponent(Scene);
+
+  CompassMesh = CreateDefaultSubobject<UStaticMeshComponent>(
+      TEXT("CompassMesh"));
+  CompassMesh->SetupAttachment(Scene);
+  ConstructorHelpers::FObjectFinder<UStaticMesh> CompassFinder(
+      TEXT("Class'/SunPosition/Editor/SM_Compass'"));
+  if (CompassFinder.Succeeded()) {
+    CompassMesh->SetStaticMesh(CompassFinder.Object);
+  }
+  CompassMesh->SetCollisionProfileName(TEXT("NoCollision"));
+  CompassMesh->CastShadow = false;
+  CompassMesh->bHiddenInGame = true;
+  CompassMesh->bIsEditorOnly = true;
+
+  SkyLight = CreateDefaultSubobject<USkyLightComponent>(TEXT("SkyLight"));
+  SkyLight->SetupAttachment(Scene);
+  SkyLight->SetRelativeLocation(FVector(0, 0, 150));
+  SkyLight->SetMobility(EComponentMobility::Movable);
+  SkyLight->bRealTimeCapture = true;
+  SkyLight->bLowerHemisphereIsBlack = false;
+  SkyLight->bTransmission = true;
+  SkyLight->bCastRaytracedShadow = true;
+  SkyLight->SamplesPerPixel = 2;
+
+  DirectionalLight = CreateDefaultSubobject<UDirectionalLightComponent>(
+      TEXT("DirectionalLight"));
+  DirectionalLight->SetupAttachment(Scene);
+  DirectionalLight->SetRelativeLocation(FVector(0, 0, 100));
+  DirectionalLight->Intensity = 111000.f;
+  DirectionalLight->LightSourceAngle = 0.5;
+  DirectionalLight->bUsedAsAtmosphereSunLight = true;
+  DirectionalLight->DynamicShadowCascades = 5;
+  DirectionalLight->CascadeDistributionExponent = 1.4;
+
+  SkyAtmosphereComponent = CreateDefaultSubobject<USkyAtmosphereComponent>(
+      TEXT("SkyAtmosphere"));
+  SkyAtmosphereComponent->SetupAttachment(Scene);
+  SkyAtmosphereComponent->TransformMode =
+      ESkyAtmosphereTransformMode::PlanetCenterAtComponentTransform;
 
   if (!Georeference) {
     Georeference = ACesiumGeoreference::GetDefaultGeoreference(this);
@@ -15,6 +58,81 @@ ACesiumSunSky::ACesiumSunSky() {
     Georeference->OnGeoreferenceUpdated.AddUniqueDynamic(
         this,
         &ACesiumSunSky::HandleGeoreferenceUpdated);
+  }
+}
+
+void ACesiumSunSky::UpdateSun_Implementation() {
+  // No C++ base implementation for now
+}
+
+void ACesiumSunSky::GetHMSFromSolarTime(
+    float InSolarTime,
+    int32& Hour,
+    int32& Minute,
+    int32& Second) {
+  Hour = FMath::TruncToInt(InSolarTime) % 24;
+  Minute = (FMath::TruncToInt(InSolarTime - Hour) * 60) % 60;
+  
+  // Convert hours + minutes so far to seconds, subtract from InSolarTime.
+  // Not exactly sure why 0.5 is added at the end. Maybe to ensure that times
+  // on the hour (e.g. 13.0) don't get truncated down after arithmetic
+  // due to floating point errors.
+  float secondsFloat = ((InSolarTime - Hour - Minute / 60) * 3600) + 0.5;
+  Second = FMath::TruncToInt(secondsFloat) % 60;
+}
+
+bool ACesiumSunSky::IsDST(
+    bool DSTEnable,
+    int32 InDSTStartMonth,
+    int32 InDSTStartDay,
+    int32 InDSTEndMonth,
+    int32 InDSTEndDay,
+    int32 InDSTSwitchHour) const {
+  if (!DSTEnable) {
+    return false;
+  }
+  int32 hour, minute, second;
+  this->GetHMSFromSolarTime(SolarTime, hour, minute, second);
+  
+  // Editor will crash if we create an invalid FDateTime, so validate these
+  // settings first
+  if (!FDateTime::Validate(Year, Month, Day, hour, minute, second, 0)) {
+    return false;
+  }
+  FDateTime current = FDateTime(Year, Month, Day, hour, minute, second);
+  FDateTime dstStart = FDateTime(
+      Year,
+      InDSTStartMonth,
+      InDSTStartDay,
+      InDSTSwitchHour);
+  FDateTime dstEnd = FDateTime(
+      Year,
+      InDSTEndMonth,
+      InDSTEndDay,
+      InDSTSwitchHour);
+  return current >= dstStart && current <= dstEnd;
+}
+
+void ACesiumSunSky::HandleGeoreferenceUpdated() {
+  if (Georeference) {
+    UE_LOG(
+        LogTemp,
+        Warning,
+        TEXT("HandleGeoreferenceUpdated entered on CesiumSunSky"));
+    this->SetActorLocation(
+        Georeference->InaccurateTransformEcefToUnreal(FVector::ZeroVector));
+    switch (Georeference->OriginPlacement) {
+    case EOriginPlacement::CartographicOrigin: {
+      FVector llh = Georeference->
+          InaccurateGetGeoreferenceOriginLongitudeLatitudeHeight();
+      this->Longitude = llh.X;
+      this->Latitude = llh.Y;
+      UpdateSun();
+      break;
+    }
+    default:
+      break;
+    }
   }
 }
 
--- Source/CesiumRuntime/Private/CesiumSunSky.h
+++ Source/CesiumRuntime/Private/CesiumSunSky.h
@@ -5,7 +5,12 @@
 #include "CoreMinimal.h"
 
 #include "CesiumGeoreference.h"
+#include "ActorFactories/ActorFactoryDirectionalLight.h"
+#include "Components/DirectionalLightComponent.h"
 #include "Components/SkyAtmosphereComponent.h"
+#include "Components/SkyLightComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/DirectionalLight.h"
 #include "GameFramework/Actor.h"
 #include "CesiumSunSky.generated.h"
 
@@ -17,14 +22,204 @@ public:
   // Sets default values for this actor's properties
   ACesiumSunSky();
 
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Components)
+  USceneComponent* Scene;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Components)
+  UStaticMeshComponent* CompassMesh;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Components)
+  USkyLightComponent* SkyLight;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Components)
+  UDirectionalLightComponent* DirectionalLight;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Components)
+  USkyAtmosphereComponent* SkyAtmosphereComponent;
+
+  /**
+   * Mobile platforms currently do not support the SkyAtmosphereComponent.
+   * In lieu of that, use the engine BP_Sky_Sphere class, or a derived class.
+   */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Mobile)
+  UClass* SkySphereClass;
+
+  /**
+   * Hold a reference to a georeference for height checks.
+   */
   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Cesium)
   ACesiumGeoreference* Georeference;
 
   /**
+   * Updates the atmosphere automatically given current player pawn's longitude, latitude, and height. Fixes artifacts seen with the atmosphere rendering when flying high above the surface, or low to the ground in high latitudes.
+   */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Cesium)
+  bool UpdateAtmosphereAtRuntime = true;
+
+  /**
+   * How frequently the atmosphere should be updated, in seconds.
+   */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Cesium)
+  float UpdateAtmospherePeriod = 1.f;
+
+  /**
+   * False: Use Directional Light component inside CesiumSunSky.
+   * True: Use the assigned Directional Light in the level.
+   */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Sun)
+  bool UseLevelDirectionalLight = false;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Sun)
+  ADirectionalLight* LevelDirectionalLight;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = Location,
+    meta=(UIMin = -89.99, UIMax = 89.99, ClampMin = -89.99, ClampMax = 89.99))
+  float Latitude = 45.f;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = Location,
+    meta=(UIMin = -180, UIMax = 180, ClampMin = -180, ClampMax = 180))
+  float Longitude = -73.f;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = Location,
+    meta=(UIMin = -12, UIMax = 14, ClampMin = -12, ClampMax = 14))
+  float TimeZone = -5.f;
+
+  UPROPERTY(
+    EditDefaultsOnly,
+    BlueprintReadWrite,
+    Category = Location,
+    meta=(UIMin = -360, UIMax = 360, ClampMin = -360, ClampMax = 360))
+  float NorthOffset = -90.f;
+
+  /** Sun elevation */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Location)
+  float Elevation = 0.f;
+
+  /** Sun elevation, corrected for atmospheric diffraction */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Location)
+  float CorrectedElevation = 0.f;
+
+  /** Sun azimuth */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Location)
+  float Azimuth = 0.f;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = -4, UIMax = 22, ClampMin = 0, ClampMax = 23.9999))
+  float SolarTime = 13.f;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1, UIMax = 31, ClampMin = 1, ClampMax = 31))
+  int32 Day = 21;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1, UIMax = 12, ClampMin = 1, ClampMax = 12))
+  int32 Month = 9;
+
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1800, UIMax = 2200, ClampMin = 0, ClampMax = 4000))
+  int32 Year = 2019;
+
+  /** Enables Daylight Saving Time (DST) */
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Date and Time")
+  bool UseDaylightSavingTime = true;
+
+
+  /** Set the Date at which DST starts in the current year */
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1, UIMax = 12, ClampMin = 1, ClampMax = 12))
+  int32 DSTStartMonth = 3;
+
+  /** Set the Date at which DST starts in the current year */
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1, UIMax = 31, ClampMin = 1, ClampMax = 31))
+  int32 DSTStartDay = 10;
+
+  /** Set the Date at which DST ends in the current year */
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1, UIMax = 12, ClampMin = 1, ClampMax = 12))
+  int32 DSTEndMonth = 11;
+
+  /** Set the Date at which DST ends in the current year */
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 1, UIMax = 31, ClampMin = 1, ClampMax = 31))
+  int32 DSTEndDay = 3;
+
+  /** Hour of the DST Switch for both beginning and end */
+  UPROPERTY(
+    EditAnywhere,
+    BlueprintReadWrite,
+    Category = "Date and Time",
+    meta=(UIMin = 0, UIMax = 23, ClampMin = 0, ClampMax = 23))
+  int32 DSTSwitchHour = 2.f;
+
+  UPROPERTY(BlueprintReadWrite)
+  float HashVal;
+
+public:
+  UFUNCTION(
+    CallInEditor,
+    BlueprintCallable,
+    BlueprintNativeEvent,
+    Category = Sun)
+  void UpdateSun();
+  void UpdateSun_Implementation();
+
+  UFUNCTION(BlueprintCallable, BlueprintPure)
+  static void GetHMSFromSolarTime(
+      float InSolarTime,
+      int32& Hour,
+      int32& Minute,
+      int32& Second);
+
+  UFUNCTION(BlueprintCallable, BlueprintPure)
+  bool IsDST(
+      bool DSTEnable,
+      int32 InDSTStartMonth,
+      int32 InDSTStartDay,
+      int32 InDSTEndMonth,
+      int32 InDSTEndDay,
+      int32 InDSTSwitchHour) const;
+
+
+protected:
+  /**
    * Callback after georeference origin (e.g. lat/long position) has been
    * updated.
    */
-  UFUNCTION(BlueprintImplementableEvent)
+  UFUNCTION(BlueprintCallable)
   void HandleGeoreferenceUpdated();
 
   /**
