a5f5fae8af7067ff55e22df3de09bd617cdfc300
Kevin Ring <kevin@kotachrome.com>
2021-07-06 11:51:02 +1000

    Missed some 21e->21bs.

--- travis/travis-get-android-ndk.sh
+++ travis/travis-get-android-ndk.sh
@@ -1,12 +1,12 @@
 if [[ $TRAVIS_OS_NAME == "windows" ]]
 then
     echo "Setting up Android NDK"
-    if [[ ! -d "$HOME/android-ndk-r21e/platforms" ]]
+    if [[ ! -d "$HOME/android-ndk-r21b/platforms" ]]
     then
         echo "Downloading Android NDK"
         wget -q https://dl.google.com/android/repository/android-ndk-r21b-windows-x86_64.zip
-        7z x android-ndk-r21e-windows-x86_64.zip "-o$HOME/"
-        rm android-ndk-r21e-windows-x86_64.zip
+        7z x android-ndk-r21b-windows-x86_64.zip "-o$HOME/"
+        rm android-ndk-r21b-windows-x86_64.zip
     else
         echo "Using Android NDK from Travis Cache"
     fi
