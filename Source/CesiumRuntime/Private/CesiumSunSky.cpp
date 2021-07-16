60ee22ef1ac73e5e47442b56cfe33ab68e1ec23b
Xuelong Mu <xuelongmu@gmail.com>
2021-07-15 23:42:21 -0400

    added copyright

--- Source/CesiumRuntime/Private/CesiumSunSky.cpp
+++ Source/CesiumRuntime/Private/CesiumSunSky.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2020-2021 CesiumGS, Inc. and Contributors
 
 #include "CesiumSunSky.h"
 #include "CesiumRuntime.h"
