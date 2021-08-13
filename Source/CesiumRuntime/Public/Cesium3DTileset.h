3e6140cc24b0bad517779a528fe4b0f7ae4f0c1f
Nithin Pranesh <nithinpra7@gmail.com>
2021-08-13 00:19:36 -0400

    send scene capture frustums to tile selection

--- Source/CesiumRuntime/Public/Cesium3DTileset.h
+++ Source/CesiumRuntime/Public/Cesium3DTileset.h
@@ -539,6 +539,7 @@ private:
 
   std::vector<UnrealCameraParameters> GetCameras() const;
   std::vector<UnrealCameraParameters> GetPlayerCameras() const;
+  std::vector<UnrealCameraParameters> GetSceneCaptures() const;
 
   /**
    * Writes the values of all properties of this actor into the
