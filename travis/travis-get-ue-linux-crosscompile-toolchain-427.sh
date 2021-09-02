876bbc95ecb9a71788fec7a4ae2f2443052f7ea1
Kevin Ring <kevin@kotachrome.com>
2021-09-02 19:01:22 +1000

    Add Linux 4.27 build (cross-compiled on Windows).

---
+++ travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
@@ -0,0 +1,4 @@
+echo "Downloading Linux toolchain"
+wget -q https://cdn.unrealengine.com/CrossToolchain_Linux/v19_clang-11.0.1-centos7.exe
+chmod a+x v19_clang-11.0.1-centos7.exe
+./v19_clang-11.0.1-centos7.exe "//S"
