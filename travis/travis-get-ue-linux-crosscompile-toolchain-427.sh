fe3d990436d6beb330bcec159fd9f157ebfc8910
Kevin Ring <kevin@kotachrome.com>
2021-09-02 20:06:01 +1000

    Diagnose download problem?

--- travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
+++ travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
@@ -1,4 +1,5 @@
 echo "Downloading Linux toolchain"
-wget -q https://cdn.unrealengine.com/CrossToolchain_Linux/v19_clang-11.0.1-centos7.exe
+wget https://cdn.unrealengine.com/CrossToolchain_Linux/v19_clang-11.0.1-centos7.exe
+echo "Finished download"
 chmod a+x v19_clang-11.0.1-centos7.exe
 ./v19_clang-11.0.1-centos7.exe "//S"
