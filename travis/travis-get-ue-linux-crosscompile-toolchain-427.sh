6aa9dc664f02dafbb5c48c16c75e9ad16ec4e22d
Kevin Ring <kevin@kotachrome.com>
2021-09-02 21:06:23 +1000

    wget doesn't like the UE CDN certificate, try httpie.

--- travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
+++ travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
@@ -1,5 +1,5 @@
 echo "Downloading Linux toolchain"
-wget https://cdn.unrealengine.com/CrossToolchain_Linux/v19_clang-11.0.1-centos7.exe
+http --download https://cdn.unrealengine.com/CrossToolchain_Linux/v19_clang-11.0.1-centos7.exe
 echo "Finished download"
 chmod a+x v19_clang-11.0.1-centos7.exe
 ./v19_clang-11.0.1-centos7.exe "//S"
