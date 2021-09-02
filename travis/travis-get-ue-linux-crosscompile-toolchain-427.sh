bf64cc4bb1009ec883ed2eb0971e6ecd375933f1
Kevin Ring <kevin@kotachrome.com>
2021-09-02 22:03:50 +1000

    Get Linux cross compile tools from our own GCS.
    Not sure why the normal download link isn't working from Travis. Maybe
    it's blocked?

--- travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
+++ travis/travis-get-ue-linux-crosscompile-toolchain-427.sh
@@ -1,5 +1,5 @@
 echo "Downloading Linux toolchain"
-http --download https://cdn.unrealengine.com/CrossToolchain_Linux/v19_clang-11.0.1-centos7.exe
+AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/4.27.0/Windows/v19_clang-11.0.1-centos7.exe .
 echo "Finished download"
 chmod a+x v19_clang-11.0.1-centos7.exe
 ./v19_clang-11.0.1-centos7.exe "//S"
