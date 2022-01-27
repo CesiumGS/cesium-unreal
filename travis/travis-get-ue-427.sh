b301dfb95eac2541a499c903e243f493d8e8b787
Kevin Ring <kevin@kotachrome.com>
2022-01-27 11:33:58 +1100

    Add UE 4.27 build on macOS.

--- travis/travis-get-ue-427.sh
+++ travis/travis-get-ue-427.sh
@@ -7,13 +7,12 @@ then
     AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/4.27.0/Windows/UE_4.27-minimal.zip .
     7z x UE_4.27-minimal.zip "-oC:\Epic"
     rm UE_4.27-minimal.zip
-# elif [[ $TRAVIS_OS_NAME == "osx" ]]
-# then
-#     aws s3 cp s3://cesium-unreal-engine/macos-2021-04-13/UE_4.26.tar.gz .
-#     mkdir $HOME/UE_4.26
-#     tar xfz UE_4.26.tar.gz -C $HOME/UE_4.26
-#     rm UE_4.26.tar.gz
-# elif [[ $TRAVIS_OS_NAME == "linux" ]]
+elif [[ $TRAVIS_OS_NAME == "osx" ]]
+then
+    aws s3 cp s3://cesium-unreal-engine/macos-2022-01-27/UE_4272_macOS.zip .
+    unzip UE_4272_macOS.zip -C $HOME
+    rm UE_4272_macOS.zip
+elif [[ $TRAVIS_OS_NAME == "linux" ]]
 # then
 #     AWS_ACCESS_KEY_ID=${GOOGLE_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${GOOGLE_SECRET_ACCESS_KEY} aws s3 --endpoint-url https://storage.googleapis.com cp s3://cesium-unreal-engine/linux-2021-04-23/UnrealEngine-4.26.2.tar.gz .
 #     mkdir $HOME/UE_4.26
