4eb38573b82e53cd97cca243074eff457950a090
Kevin Ring <kevin@kotachrome.com>
2022-02-28 17:12:32 +1100

    Install first because bake doesn't build.

--- travis/travis-setup-native.sh
+++ travis/travis-setup-native.sh
@@ -4,4 +4,5 @@ python ./tools/automate.py conan-export-recipes
 python ./tools/automate.py generate-library-recipes
 python ./tools/automate.py conan-export-libraries
 cd ../../Source/CesiumNative
+conan install . --build=outdated --build=cascade -pr:b=$CONAN_PROFILE -pr:h=$CONAN_PROFILE -s build_type=Release
 ue4 conan bake $CONAN_PROFILE
