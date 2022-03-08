c51add87fbbb7e22bb1f250cb40f9fd0f849770d
Nithin Pranesh <nithinpra7@gmail.com>
2022-03-08 11:16:36 -0500

    generates test material function, added feature table access type

--- Source/CesiumRuntime/Private/CesiumEncodedMetadataComponent.cpp
+++ Source/CesiumRuntime/Private/CesiumEncodedMetadataComponent.cpp
@@ -6,6 +6,20 @@
 #include "CesiumMetadataConversions.h"
 #include "CesiumMetadataModel.h"
 
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "ComponentReregisterContext.h"
+#include "Factories/MaterialFactoryNew.h"
+#include "Factories/MaterialFunctionMaterialLayerFactory.h"
+#include "Materials/Material.h"
+#include "Materials/MaterialExpressionCustom.h"
+#include "Materials/MaterialExpressionScalarParameter.h"
+#include "Materials/MaterialExpressionTextureProperty.h"
+#include "Materials/MaterialExpressionTextureSampleParameter.h"
+#include "Materials/MaterialFunctionMaterialLayer.h"
+#include "UObject/Package.h"
+
+#include "Materials/MaterialExpressionTextureCoordinate.h"
+
 void UCesiumEncodedMetadataComponent::AutoFill() {
   const ACesium3DTileset* pOwner = this->GetOwner<ACesium3DTileset>();
   if (!pOwner) {
@@ -131,3 +145,113 @@ void UCesiumEncodedMetadataComponent::AutoFill() {
     }
   }
 }
+
+#if WITH_EDITOR
+static UMaterialExpressionCustom* CreateSelectTexCoords(
+    UMaterialFunctionMaterialLayer* UnrealMaterial,
+    UMaterialExpressionScalarParameter* TexCoordIndex) {
+  // TODO: this is a temporary substitute
+  // for figuring out how to load the existing Cesium material function
+
+  TArray<UMaterialExpressionTextureCoordinate*> TexCoords;
+  TexCoords.SetNum(8);
+  for (int32 i = 0; i < 8; ++i) {
+    UMaterialExpressionTextureCoordinate* TexCoord =
+        NewObject<UMaterialExpressionTextureCoordinate>(UnrealMaterial);
+    TexCoord->CoordinateIndex = i;
+    UnrealMaterial->FunctionExpressions.Add(TexCoord);
+    TexCoords[i] = TexCoord;
+  }
+
+  UMaterialExpressionCustom* Select =
+      NewObject<UMaterialExpressionCustom>(UnrealMaterial);
+  Select->OutputType = ECustomMaterialOutputType::CMOT_Float2;
+  Select->Inputs.SetNum(9);
+
+  Select->Inputs[0].InputName = "TexCoordIndex";
+  Select->Inputs[0].Input.Expression = TexCoordIndex;
+
+  for (int32 i = 0; i < 8; ++i) {
+    Select->Inputs[i + 1].InputName = FName("TexCoord" + FString::FromInt(i));
+    Select->Inputs[i + 1].Input.Expression = TexCoords[i];
+  }
+
+  Select->Code =
+      "float2 texCoords[8] = {TexCoord0, TexCoord1, TexCoord2, TexCoord3, TexCoord4, TexCoord5, TexCoord6, TexCoord7}; int index = TexCoordIndex; return texCoords[index];";
+
+  UnrealMaterial->FunctionExpressions.Add(Select);
+  return Select;
+}
+
+template <typename ObjClass>
+static FORCEINLINE ObjClass* LoadObjFromPath(const FName& Path) {
+  if (Path == NAME_None)
+    return nullptr;
+
+  return Cast<ObjClass>(
+      StaticLoadObject(ObjClass::StaticClass(), nullptr, *Path.ToString()));
+}
+
+static FORCEINLINE UMaterial* LoadMaterialFromPath(const FName& Path) {
+  if (Path == NAME_None)
+    return nullptr;
+
+  return LoadObjFromPath<UMaterial>(Path);
+}
+
+void UCesiumEncodedMetadataComponent::GenerateMaterial() {
+  FString MaterialBaseName = "ML_Material";
+  FString PackageName = "/Game/";
+  PackageName += MaterialBaseName;
+  UPackage* Package = CreatePackage(NULL, *PackageName);
+
+  // Create an unreal material asset
+  auto MaterialFactory = NewObject<UMaterialFunctionMaterialLayerFactory>();
+  UMaterialFunctionMaterialLayer* UnrealMaterial =
+      (UMaterialFunctionMaterialLayer*)MaterialFactory->FactoryCreateNew(
+          UMaterialFunctionMaterialLayer::StaticClass(),
+          Package,
+          *MaterialBaseName,
+          RF_Standalone | RF_Public,
+          NULL,
+          GWarn);
+  FAssetRegistryModule::AssetCreated(UnrealMaterial);
+  Package->FullyLoad();
+  Package->SetDirtyFlag(true);
+
+  UMaterialExpressionScalarParameter* TexCoordsIndex =
+      NewObject<UMaterialExpressionScalarParameter>(UnrealMaterial);
+  TexCoordsIndex->ParameterName = "FeatureIdTexture:0_TextureCoordinateIndex";
+  TexCoordsIndex->DefaultValue = 0.0f;
+  UnrealMaterial->FunctionExpressions.Add(TexCoordsIndex);
+
+  UMaterialExpressionCustom* SelectTexCoords =
+      CreateSelectTexCoords(UnrealMaterial, TexCoordsIndex);
+
+  UMaterial *
+
+      // let the material update itself if necessary
+      UnrealMaterial->PreEditChange(NULL);
+  UnrealMaterial->PostEditChange();
+
+  // make sure that any static meshes, etc using this material will stop using
+  // the FMaterialResource of the original material, and will use the new
+  // FMaterialResource created when we make a new UMaterial in place
+  FGlobalComponentReregisterContext RecreateComponents;
+
+  /*
+    UMaterialExpressionTextureSampleParameter* FeatureIdTexture =
+        NewObject<MaterialExpressionTextureSampleParameter>(UnrealMaterial);
+    FeatureIdTexture->ParameterName = "FeatureIdTexture:0_Texture";
+    UnrealMaterial->Expressions.Add(FeatureIdTexture);
+
+    UMaterialExpressionTextureSampleParameter* FeatureTable =
+        NewObject<MaterialExpressionTextureSampleParameter>(UnrealMaterial);
+    FeatureTable->ParameterName = "FeatureTable:labels_attribute";
+    UnrealMaterial->Expressions.Add(FeatureTable);
+
+    UMaterialExpressionCustom* PropertyArrayLookup =
+        NewObject<UMaterialExpressionCustom>(UnrealMaterial);
+    FeatureTableLookup->OutputType = ECustomMaterialOutputType::CMOT_Float1;*/
+}
+#endif
--- Source/CesiumRuntime/Public/CesiumEncodedMetadataComponent.h
+++ Source/CesiumRuntime/Public/CesiumEncodedMetadataComponent.h
@@ -12,6 +12,14 @@ enum class ECesiumPropertyComponentType : uint8 { Uint8, Float };
 UENUM()
 enum class ECesiumPropertyType : uint8 { Scalar, Vec2, Vec3, Vec4 };
 
+ENUM()
+enum class ECesiumFeatureTableAccessType : uint8 {
+  Unknown,
+  Texture,
+  Attribute,
+  Mixed
+};
+
 // Note that these don't exhaustively cover the possibilities of glTF metadata
 // classes, they only cover the subset that can be encoded into textures. For
 // example, arbitrary size arrays and strings are excluded.
@@ -43,6 +51,10 @@ struct CESIUMRUNTIME_API FFeatureTableDescription {
   FString Name;
 
   UPROPERTY(EditAnywhere, Category = "Cesium")
+  ECesiumFeatureTableAccessType AccessType =
+      ECesiumFeatureTableAccessType::Unknown;
+
+  UPROPERTY(EditAnywhere, Category = "Cesium")
   TArray<FPropertyDescription> Properties;
 };
 
@@ -60,12 +72,20 @@ class CESIUMRUNTIME_API UCesiumEncodedMetadataComponent
   GENERATED_BODY()
 
 public:
-  UPROPERTY(EditAnywhere, Category = "Cesium")
+  UPROPERTY(EditAnywhere, Category = "EncodeMetadata")
   TArray<FFeatureTableDescription> FeatureTables;
 
-  UPROPERTY(EditAnywhere, Category = "Cesium")
+  UPROPERTY(EditAnywhere, Category = "EncodeMetadata")
   TArray<FFeatureTextureDescription> FeatureTextures;
 
-  UFUNCTION(CallInEditor, Category = "Cesium")
+  UFUNCTION(CallInEditor, Category = "EncodeMetadata")
   void AutoFill();
+
+#if WITH_EDITOR
+  UPROPERTY(EditAnywhere, Category = "GenerateMaterial")
+  int32 FeatureIdAttributes;
+
+  UFUNCTION(CallInEditor, Category = "GenerateMaterial")
+  void GenerateMaterial();
+#endif
 };
