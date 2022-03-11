68cf97c8aa194a655978298345b62d8feb7d44a8
Kevin Ring <kevin@kotachrome.com>
2022-03-11 23:32:32 +1100

    Fix profile path.

--- travis/travis-setup-native.sh
+++ travis/travis-setup-native.sh
@@ -11,7 +11,7 @@ pushd extern
 $CESIUM_PYTHON ./cesium-native/tools/automate.py conan-export-recipes
 $CESIUM_PYTHON ./cesium-native/tools/automate.py generate-library-recipes --editable
 $CESIUM_PYTHON ./cesium-native/tools/automate.py editable on
-conan install . -pr:b=default -pr:h=extern/conan-profiles/$CONAN_PROFILE.jinja
+conan install . -pr:b=default -pr:h=./conan-profiles/$CONAN_PROFILE.jinja
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cesium-native/build/CesiumUtility/conan/conan_toolchain.cmake
 cmake --build build --target install -j 4
 popd
