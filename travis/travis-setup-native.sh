0434d85c4048b57e84f0e6e5d0636073706d8571
Kevin Ring <kevin@kotachrome.com>
2022-03-14 23:02:54 +1100

    Fix up Linux build, hopefully.

--- travis/travis-setup-native.sh
+++ travis/travis-setup-native.sh
@@ -1,17 +1,13 @@
 source travis/travis-find-python.sh
 
-# Travis defines CC and CXX to gcc on Windows, for some reason.
-# We need to undo that, or conan will build with the wrong compiler.
-export CC=
-export CXX=
-
 $CESIUM_PIP install conan
+
 
 pushd extern
 $CESIUM_PYTHON ./cesium-native/tools/automate.py conan-export-recipes
 $CESIUM_PYTHON ./cesium-native/tools/automate.py generate-library-recipes --editable
 $CESIUM_PYTHON ./cesium-native/tools/automate.py editable on
 conan install . -pr:b=default -pr:h=./conan-profiles/$CONAN_PROFILE.jinja --build=outdated --build=cascade
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cesium-native/build/CesiumUtility/conan/conan_toolchain.cmake
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cesium-native/build/CesiumUtility/conan/conan_toolchain.cmake $EXTRA_CONFIGURE_ARGS
 cmake --build build --target install -j 4 --config Release
 popd
