b5916e74028c880ff6e5b832fe0f195b5d449b9f
Kevin Ring <kevin@kotachrome.com>
2022-03-16 10:37:32 +1100

    Force build of uriparser.
    Because the Conan Center version was built on a system with
    reallocarray, and Unreal's CentOS7 toolchain does not have this
    function. So we get linker errors if we use the Conan Center version.

--- travis/travis-setup-native.sh
+++ travis/travis-setup-native.sh
@@ -9,7 +9,8 @@ $CESIUM_PYTHON ./cesium-native/tools/automate.py generate-library-recipes --edit
 conan editable add conan/recipes/openssl-ue4.26 openssl/UE4.26
 conan editable add conan/recipes/openssl-ue4.27 openssl/UE4.27
 $CESIUM_PYTHON ./cesium-native/tools/automate.py editable on
-conan install . -pr:b=default -pr:h=./conan-profiles/$CONAN_PROFILE.jinja --build=outdated --build=cascade
+# Force build of uriparser because the Conan Center version uses reallocarray, which does not exist in UE's CentOS 7
+conan install . -pr:b=default -pr:h=./conan-profiles/$CONAN_PROFILE.jinja --build=uriparser --build=outdated --build=cascade
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cesium-native/build/CesiumUtility/conan/conan_toolchain.cmake $EXTRA_CONFIGURE_ARGS
 cmake --build build --target install -j 4 --config Release
 popd
