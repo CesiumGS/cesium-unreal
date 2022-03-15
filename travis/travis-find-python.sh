a9a195f3326e9b4ae10e79a75f3e27a65fc5d165
Kevin Ring <kevin@kotachrome.com>
2022-03-15 12:17:45 +1100

    Verify python works, set correct env for Linux 4.26.

--- travis/travis-find-python.sh
+++ travis/travis-find-python.sh
@@ -1,15 +1,16 @@
 # On some platforms, it's python3, on others it's just python
 export CESIUM_PYTHON=python3
-if ! command -v python3
+if (! command -v python3) || (! python3 --version)
 then
   export CESIUM_PYTHON=python
 fi
 
 export CESIUM_PIP=pip3
-if ! command -v pip3
+if (! command -v pip3) || (! pip3 --version)
 then
   export CESIUM_PIP=pip
 fi
 
+echo Python executable: $CESIUM_PYTHON
 $CESIUM_PYTHON --version
 $CESIUM_PIP --version
