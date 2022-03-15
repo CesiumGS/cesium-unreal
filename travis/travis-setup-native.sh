51d45013d18c717e35524cb3b1543e01defc3a5d
Kevin Ring <kevin@kotachrome.com>
2022-03-15 18:05:45 +1100

    Don't try to invoke conan with python.

--- travis/travis-setup-native.sh
+++ travis/travis-setup-native.sh
@@ -6,8 +6,8 @@ $CESIUM_PIP install conan
 pushd extern
 $CESIUM_PYTHON ./cesium-native/tools/automate.py conan-export-recipes
 $CESIUM_PYTHON ./cesium-native/tools/automate.py generate-library-recipes --editable
-$CESIUM_PYTHON conan editable add conan/recipes/openssl-ue4.26 openssl/UE4.26
-$CESIUM_PYTHON conan editable add conan/recipes/openssl-ue4.27 openssl/UE4.27
+conan editable add conan/recipes/openssl-ue4.26 openssl/UE4.26
+conan editable add conan/recipes/openssl-ue4.27 openssl/UE4.27
 $CESIUM_PYTHON ./cesium-native/tools/automate.py editable on
 conan install . -pr:b=default -pr:h=./conan-profiles/$CONAN_PROFILE.jinja --build=outdated --build=cascade
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cesium-native/build/CesiumUtility/conan/conan_toolchain.cmake $EXTRA_CONFIGURE_ARGS
