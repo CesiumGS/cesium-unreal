c619d992c77b1789ed9137d48000af106d1cb78b
Kevin Ring <kevin@kotachrome.com>
2022-03-15 12:12:51 +1100

    Fix Linux compile problems.

--- extern/unreal-linux-toolchain.cmake
+++ extern/unreal-linux-toolchain.cmake
@@ -15,13 +15,13 @@ endif()
 
 set(CMAKE_C_COMPILER_TARGET x86_64-unknown-linux-gnu)
 set(CMAKE_CXX_COMPILER_TARGET x86_64-unknown-linux-gnu)
-string(APPEND CMAKE_CXX_FLAGS_INIT "\"-isystem$ENV{UNREAL_ENGINE_DIR}/Engine/Source/ThirdParty/Linux/LibCxx/include\" \"-isystem$ENV{UNREAL_ENGINE_DIR}/Engine/Source/ThirdParty/Linux/LibCxx/include/c++/v1\" -target x86_64-unknown-linux-gnu -Qunused-arguments -pthread")
-string(APPEND CMAKE_C_FLAGS_INIT "-target x86_64-unknown-linux-gnu -pthread")
-string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "\"-L$ENV{UNREAL_ENGINE_DIR}/Engine/Source/ThirdParty/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu\" -pthread")
+string(APPEND CMAKE_CXX_FLAGS_INIT "\"-isystem$ENV{UNREAL_ENGINE_DIR}/Engine/Source/ThirdParty/Linux/LibCxx/include\" \"-isystem$ENV{UNREAL_ENGINE_DIR}/Engine/Source/ThirdParty/Linux/LibCxx/include/c++/v1\" -target x86_64-unknown-linux-gnu -Qunused-arguments -pthread ")
+string(APPEND CMAKE_C_FLAGS_INIT "-target x86_64-unknown-linux-gnu -pthread ")
+string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "\"-L$ENV{UNREAL_ENGINE_DIR}/Engine/Source/ThirdParty/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu\" -pthread ")
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
