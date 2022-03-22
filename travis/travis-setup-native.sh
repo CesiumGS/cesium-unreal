f4a8827ea0619e52340368cfd4717ca6cc650d2c
Kevin Ring <kevin@kotachrome.com>
2022-03-22 18:05:05 +1100

    Back to external conan install.

--- travis/travis-setup-native.sh
+++ travis/travis-setup-native.sh
@@ -10,6 +10,8 @@ conan editable add conan/recipes/openssl-ue4.26 openssl/UE4.26
 conan editable add conan/recipes/openssl-ue4.27 openssl/UE4.27
 $CESIUM_PYTHON ./cesium-native/tools/automate.py editable on
 mkdir build
-eval "cmake -B build -S . -DCMAKE_BUILD_TYPE=Release $EXTRA_CONFIGURE_ARGS"
+# Force build of uriparser because the Conan Center version uses reallocarray, which does not exist in UE's CentOS 7
+conan install . -pr:b=default -pr:h=./conan-profiles/$CONAN_PROFILE.jinja --build=uriparser --build=outdated --build=cascade
+eval "cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=cesium-native/build/CesiumUtility/conan/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release $EXTRA_CONFIGURE_ARGS"
 cmake --build build --target install -j 4 --config Release
 popd
