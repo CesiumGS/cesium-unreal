3ecd16b62cd1155df436e0aef5b512d60bb61942
Nithin Pranesh <nithinpra7@gmail.com>
2022-03-24 12:12:16 -0400

    restore old version of CesiumMetadataFeatureTable (will deprecate this)

--- Source/CesiumRuntime/Public/CesiumMetadataFeatureTable.h
+++ Source/CesiumRuntime/Public/CesiumMetadataFeatureTable.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "CesiumGltf/AccessorView.h"
 #include "CesiumMetadataGenericValue.h"
 #include "CesiumMetadataProperty.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
@@ -10,6 +11,7 @@
 
 namespace CesiumGltf {
 struct Model;
+struct Accessor;
 struct FeatureTable;
 } // namespace CesiumGltf
 
@@ -22,6 +24,15 @@ USTRUCT(BlueprintType)
 struct CESIUMRUNTIME_API FCesiumMetadataFeatureTable {
   GENERATED_USTRUCT_BODY()
 
+  using FeatureIDAccessorType = std::variant<
+      std::monostate,
+      CesiumGltf::AccessorView<CesiumGltf::AccessorTypes::SCALAR<int8_t>>,
+      CesiumGltf::AccessorView<CesiumGltf::AccessorTypes::SCALAR<uint8_t>>,
+      CesiumGltf::AccessorView<CesiumGltf::AccessorTypes::SCALAR<int16_t>>,
+      CesiumGltf::AccessorView<CesiumGltf::AccessorTypes::SCALAR<uint16_t>>,
+      CesiumGltf::AccessorView<CesiumGltf::AccessorTypes::SCALAR<uint32_t>>,
+      CesiumGltf::AccessorView<CesiumGltf::AccessorTypes::SCALAR<float>>>;
+
 public:
   /**
    * Construct an empty feature table.
@@ -32,13 +43,16 @@ public:
    * Constructs a feature table from a glTF Feature Table.
    *
    * @param Model The model that stores EXT_feature_metadata.
+   * @param Accessor The accessor for the feature ID.
    * @param FeatureTable The feature table that is paired with the feature ID.
    */
   FCesiumMetadataFeatureTable(
       const CesiumGltf::Model& Model,
+      const CesiumGltf::Accessor& Accessor,
       const CesiumGltf::FeatureTable& FeatureTable);
 
 private:
+  FeatureIDAccessorType _featureIDAccessor;
   TMap<FString, FCesiumMetadataProperty> _properties;
 
   friend class UCesiumMetadataFeatureTableBlueprintLibrary;
@@ -60,6 +74,17 @@ public:
   static int64
   GetNumberOfFeatures(UPARAM(ref)
                           const FCesiumMetadataFeatureTable& FeatureTable);
+
+  /**
+   * Gets the feature ID associated with a given vertex.
+   */
+  UFUNCTION(
+      BlueprintCallable,
+      BlueprintPure,
+      Category = "Cesium|Metadata|FeatureTable")
+  static int64 GetFeatureIDForVertex(
+      UPARAM(ref) const FCesiumMetadataFeatureTable& FeatureTable,
+      int64 VertexIndex);
 
   /**
    * Gets a map of property name to property value for a given feature.
