33606d53744850523add671c893b451d3d083132
Alex Gallegos <39537389+argallegos@users.noreply.github.com>
2022-06-06 13:27:39 -0400

    swap lat and long in sublevel display

--- Source/CesiumRuntime/Public/CesiumSubLevel.h
+++ Source/CesiumRuntime/Public/CesiumSubLevel.h
@@ -39,16 +39,6 @@ struct FCesiumSubLevel {
   bool Enabled = true;
 
   /**
-   * The WGS84 longitude in degrees of where this level should sit on the
-   * globe, in the range [-180, 180]
-   */
-  UPROPERTY(
-      EditAnywhere,
-      Category = "Cesium",
-      meta = (ClampMin = -180.0, ClampMax = 180.0, EditCondition = "Enabled"))
-  double LevelLongitude = 0.0;
-
-  /**
    * The WGS84 latitude in degrees of where this level should sit on the globe,
    * in the range [-90, 90]
    */
@@ -57,6 +47,16 @@ struct FCesiumSubLevel {
       Category = "Cesium",
       meta = (ClampMin = -90.0, ClampMax = 90.0, EditCondition = "Enabled"))
   double LevelLatitude = 0.0;
+
+    /**
+   * The WGS84 longitude in degrees of where this level should sit on the
+   * globe, in the range [-180, 180]
+   */
+  UPROPERTY(
+      EditAnywhere,
+      Category = "Cesium",
+      meta = (ClampMin = -180.0, ClampMax = 180.0, EditCondition = "Enabled"))
+  double LevelLongitude = 0.0;
 
   /**
    * The height in meters above the WGS84 globe this level should sit.
