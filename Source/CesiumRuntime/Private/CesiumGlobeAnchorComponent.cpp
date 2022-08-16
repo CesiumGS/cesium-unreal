730948a8073eb1dd7e8649ac184c151cc5bf95e1
Joseph Kaile <joseph.kaile@simblocks.io>
2022-08-16 18:05:07 -0400

    format the code

--- Source/CesiumRuntime/Private/CesiumGlobeAnchorComponent.cpp
+++ Source/CesiumRuntime/Private/CesiumGlobeAnchorComponent.cpp
@@ -397,10 +397,11 @@ void UCesiumGlobeAnchorComponent::OnUnregister() {
   USceneComponent* pOwnerRoot = pOwner->GetRootComponent();
   if (pOwnerRoot) {
     pOwnerRoot->TransformUpdated.RemoveAll(this);
-    _onTransformChangedWhileUnregistered = pOwnerRoot->TransformUpdated.AddLambda(
-        [this](USceneComponent*, EUpdateTransformFlags, ETeleportType) {
-          this->_actorToECEFIsValid = false;
-        });
+    _onTransformChangedWhileUnregistered =
+        pOwnerRoot->TransformUpdated.AddLambda(
+            [this](USceneComponent*, EUpdateTransformFlags, ETeleportType) {
+              this->_actorToECEFIsValid = false;
+            });
   }
 }
 
