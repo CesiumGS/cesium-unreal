2ab3ae1b5b9d565396b2351ae5d15a868284c63a
Kevin Ring <kevin@kotachrome.com>
2023-05-18 12:37:19 +1000

    Don't try to find CesiumCreditSystemBP_C so early.

--- Source/CesiumRuntime/Private/CesiumCreditSystemBPLoader.cpp
+++ Source/CesiumRuntime/Private/CesiumCreditSystemBPLoader.cpp
@@ -3,9 +3,4 @@
 #include "CesiumCreditSystemBPLoader.h"
 #include "UObject/ConstructorHelpers.h"
 
-UCesiumCreditSystemBPLoader::UCesiumCreditSystemBPLoader()
-    : CesiumCreditSystemBP(
-          ConstructorHelpers::FObjectFinder<UClass>(
-              TEXT(
-                  "Class'/CesiumForUnreal/CesiumCreditSystemBP.CesiumCreditSystemBP_C'"))
-              .Object) {}
+UCesiumCreditSystemBPLoader::UCesiumCreditSystemBPLoader() {}
