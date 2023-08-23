4db30315ebf83fb1958e9642763243bbb75fdd63
Brian L <130494071+csciguy8@users.noreply.github.com>
2023-08-23 13:48:20 -0600

    Set up programmatic scene with typical Cesium objects

--- Source/CesiumRuntime/Private/Tests/CesiumLoadTest.cpp
+++ Source/CesiumRuntime/Private/Tests/CesiumLoadTest.cpp
@@ -2,16 +2,21 @@
 
 #if WITH_EDITOR
 
-#include "EngineAnalytics.h"
-#include "Interfaces/IAnalyticsProvider.h"
 #include "Misc/AutomationTest.h"
 #include "Tests/AutomationCommon.h"
 #include "Tests/AutomationEditorCommon.h"
 
-#include "CesiumRuntime.h"
 #include "Editor.h"
 #include "Engine/World.h"
 #include "EngineUtils.h"
+
+#include "Cesium3DTileset.h"
+#include "CesiumCameraManager.h"
+#include "CesiumGeoreference.h"
+#include "CesiumRuntime.h"
+#include "CesiumSunSky.h"
+#include "CesiumTestHelpers.h"
+#include "GlobeAwareDefaultPawn.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(
     FCesiumLoadTest,
@@ -19,67 +24,45 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(
     EAutomationTestFlags::EditorContext | EAutomationTestFlags::PerfFilter)
 
 bool FCesiumLoadTest::RunTest(const FString& Parameters) {
-  FString MapName = "MyMap";
-  double MapLoadStartTime = 0;
 
-  // Test event for analytics. This should fire anytime this automation
-  // procedure is started.
+  //
+  // Programmatically set up the world
+  //
 
-  if (FEngineAnalytics::IsAvailable()) {
-    FEngineAnalytics::GetProvider().RecordEvent(TEXT("Editor.Usage.TestEvent"));
-    UE_LOG(
-        LogCesium,
-        Log,
-        TEXT(
-            "AnayticsTest: Load All Maps automation triggered and Editor.Usage.TestEvent analytic event has been fired."));
-  }
+  UWorld* world = CesiumTestHelpers::getGlobalWorldContext();
+  ULevel* level = world->GetCurrentLevel();
 
-  {
-    // Find the main editor window
-    TArray<TSharedRef<SWindow>> AllWindows;
-    FSlateApplication::Get().GetAllVisibleWindowsOrdered(AllWindows);
-    if (AllWindows.Num() == 0) {
-      UE_LOG(
-          LogCesium,
-          Error,
-          TEXT("ERROR: Could not find the main editor window."));
-      return false;
-    }
-    WindowScreenshotParameters WindowParameters;
-    WindowParameters.CurrentWindow = AllWindows[0];
+  // Create a camera
+  ACesiumCameraManager* cameraManager =
+      ACesiumCameraManager::GetDefaultCameraManager(world);
+  TestNotNull("Camera manager pointer is valid", cameraManager);
 
-    // Disable Eye Adaptation
-    static IConsoleVariable* CVar = IConsoleManager::Get().FindConsoleVariable(
-        TEXT("r.EyeAdaptationQuality"));
-    CVar->Set(0);
+  // Create world objects
+  ACesiumGeoreference* georeference =
+      ACesiumGeoreference::GetDefaultGeoreference(world);
+  ACesiumSunSky* sunSkyActor = world->SpawnActor<ACesiumSunSky>();
+  ACesium3DTileset* tilesetActor = world->SpawnActor<ACesium3DTileset>();
 
-    // Create a screen shot filename and path
-    const FString LoadAllMapsTestName = FString::Printf(
-        TEXT("LoadAllMaps_Editor/%s"),
-        *FPaths::GetBaseFilename(MapName));
-    WindowParameters.ScreenshotName =
-        AutomationCommon::GetScreenshotName(LoadAllMapsTestName);
+  FSoftObjectPath objectPath(
+      TEXT("Class'/CesiumForUnreal/DynamicPawn.DynamicPawn_C'"));
+  TSoftObjectPtr<UObject> DynamicPawn = TSoftObjectPtr<UObject>(objectPath);
+  AGlobeAwareDefaultPawn* pawnActor = world->SpawnActor<AGlobeAwareDefaultPawn>(
+      Cast<UClass>(DynamicPawn.LoadSynchronous()));
 
-    // Get the current number of seconds.  This will be used to track how long
-    // it took to load the map.
-    MapLoadStartTime = FPlatformTime::Seconds();
-    // Load the map
-    FAutomationEditorCommonUtils::LoadMap(MapName);
-    // Log how long it took to launch the map.
-    UE_LOG(
-        LogEditorAutomationTests,
-        Display,
-        TEXT("Map '%s' took %.3f to load"),
-        *MapName,
-        FPlatformTime::Seconds() - MapLoadStartTime);
+  TestNotNull("Georeference pointer is valid", georeference);
+  TestNotNull("SunSky actor pointer is valid", sunSkyActor);
+  TestNotNull("Tileset actor pointer is valid", tilesetActor);
+  TestNotNull("Dynamic pawn actor pointer is valid", pawnActor);
 
-    // If we don't have NoTextureStreaming enabled, give the textures some time
-    // to load.
-    if (!FParse::Param(FCommandLine::Get(), TEXT("NoTextureStreaming"))) {
-      // Give the contents some time to load
-      ADD_LATENT_AUTOMATION_COMMAND(FWaitLatentCommand(1.5f));
-    }
-  }
+  // Configure similar to Google Tiles sample
+  FVector targetOrigin(-122.083969, 37.424492, 142.859116);
+  FString targetUrl(
+      "https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyCnRPXWDIj1LuX6OWIweIqZFHHoXVgdYss");
+
+  georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(targetOrigin);
+  tilesetActor->SetUrl(targetUrl);
+  tilesetActor->SetTilesetSource(ETilesetSource::FromUrl);
+  pawnActor->SetActorRotation(FRotator(-25, 95, 0));
 
   return true;
 }
