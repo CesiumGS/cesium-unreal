a89e5a95088dd73dd3f576c5e91cf879e209f27a
Brian L <130494071+csciguy8@users.noreply.github.com>
2023-08-24 15:54:00 -0600

    Setup denver (ion) example. Steps towards ticking world after objects created.

--- Source/CesiumRuntime/Private/Tests/CesiumLoadTest.cpp
+++ Source/CesiumRuntime/Private/Tests/CesiumLoadTest.cpp
@@ -23,12 +23,105 @@
 // For debugging, it may help to create the scene in the editor
 // After the test is run, you can play with their settings and even run PIE
 //
-#define CREATE_TEST_IN_EDITOR_WORLD 0
+#define CREATE_TEST_IN_EDITOR_WORLD 1
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(
     FCesiumLoadTest,
     "Cesium.LoadTest",
     EAutomationTestFlags::EditorContext | EAutomationTestFlags::PerfFilter)
+
+void tickWorld(UWorld* world, double time) {
+  const double minStepTime = 0.001; // Don't loop faster than 100 fps
+
+  const double testStartMark = FPlatformTime::Seconds();
+  const double testEndMark = testStartMark + time;
+  double lastTimeMark = testStartMark;
+
+  while (1) {
+    double frameTimeMark = FPlatformTime::Seconds();
+
+    if (frameTimeMark > testEndMark)
+      break;
+
+    double frameElapsedTime = frameTimeMark - lastTimeMark;
+
+    if (frameElapsedTime < minStepTime) {
+      double sleepTarget = minStepTime - frameElapsedTime;
+      FPlatformProcess::Sleep(sleepTarget);
+      continue;
+    }
+
+    // Let world tick at same rate as this loop
+    world->Tick(ELevelTick::LEVELTICK_All, frameElapsedTime);
+
+    // Derived from TimerManagerTests.cpp, TimerTest_TickWorld
+    GFrameCounter++;
+
+    lastTimeMark = frameTimeMark;
+  };
+}
+
+void setupForGoogleTiles(UWorld* world) {
+  ACesiumCameraManager* cameraManager =
+      ACesiumCameraManager::GetDefaultCameraManager(world);
+  ACesiumGeoreference* georeference =
+      ACesiumGeoreference::GetDefaultGeoreference(world);
+  ACesiumSunSky* sunSky = world->SpawnActor<ACesiumSunSky>();
+  ACesium3DTileset* tileset = world->SpawnActor<ACesium3DTileset>();
+  APlayerStart* playerStart = world->SpawnActor<APlayerStart>();
+
+  FSoftObjectPath objectPath(
+      TEXT("Class'/CesiumForUnreal/DynamicPawn.DynamicPawn_C'"));
+  TSoftObjectPtr<UObject> DynamicPawn = TSoftObjectPtr<UObject>(objectPath);
+  AGlobeAwareDefaultPawn* pawn = world->SpawnActor<AGlobeAwareDefaultPawn>(
+      Cast<UClass>(DynamicPawn.LoadSynchronous()));
+  pawn->AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+  FVector targetOrigin(-122.083969, 37.424492, 142.859116);
+  FString targetUrl(
+      "https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyCnRPXWDIj1LuX6OWIweIqZFHHoXVgdYss");
+
+  georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(targetOrigin);
+  tileset->SetUrl(targetUrl);
+  tileset->SetTilesetSource(ETilesetSource::FromUrl);
+  pawn->SetActorLocation(FVector(0, 0, 0));
+  pawn->SetActorRotation(FRotator(-25, 95, 0));
+}
+
+void setupForDenver(UWorld* world) {
+  ACesiumCameraManager* cameraManager =
+      ACesiumCameraManager::GetDefaultCameraManager(world);
+  ACesiumGeoreference* georeference =
+      ACesiumGeoreference::GetDefaultGeoreference(world);
+  ACesiumSunSky* sunSky = world->SpawnActor<ACesiumSunSky>();
+  ACesium3DTileset* worldTileset = world->SpawnActor<ACesium3DTileset>();
+  ACesium3DTileset* denverTileset = world->SpawnActor<ACesium3DTileset>();
+  APlayerStart* playerStart = world->SpawnActor<APlayerStart>();
+
+  FSoftObjectPath objectPath(
+      TEXT("Class'/CesiumForUnreal/DynamicPawn.DynamicPawn_C'"));
+  TSoftObjectPtr<UObject> DynamicPawn = TSoftObjectPtr<UObject>(objectPath);
+  AGlobeAwareDefaultPawn* pawn = world->SpawnActor<AGlobeAwareDefaultPawn>(
+      Cast<UClass>(DynamicPawn.LoadSynchronous()));
+  pawn->AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+  FVector targetOrigin(-104.988892, 39.743462, 1798.679443);
+  FString ionToken(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NmZhZTk4NS01MDFmLTRjODgtOTlkYy04NjIwODhiZWExOGYiLCJpZCI6MjU5LCJpYXQiOjE2ODg1MTI4ODd9.haoe5hsJyfHk1dQAHVK6N8dW_kfmtdbyuhlGwFdEHbM");
+
+  georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(targetOrigin);
+  worldTileset->SetTilesetSource(ETilesetSource::FromCesiumIon);
+  worldTileset->SetIonAssetID(1);
+  worldTileset->SetIonAccessToken(ionToken);
+
+  denverTileset->SetTilesetSource(ETilesetSource::FromCesiumIon);
+  denverTileset->SetIonAssetID(354307);
+  denverTileset->SetIonAccessToken(ionToken);
+  denverTileset->SetMaximumScreenSpaceError(2.0);
+
+  pawn->SetActorLocation(FVector(0, 0, 0));
+  pawn->SetActorRotation(FRotator(-5.2, -149.4, 0));
+}
 
 bool FCesiumLoadTest::RunTest(const FString& Parameters) {
 
@@ -45,40 +138,17 @@ bool FCesiumLoadTest::RunTest(const FString& Parameters) {
 #endif
   TestNotNull("world is valid", world);
 
-  // Create world objects
-  ACesiumCameraManager* cameraManager =
-      ACesiumCameraManager::GetDefaultCameraManager(world);
-  ACesiumGeoreference* georeference =
-      ACesiumGeoreference::GetDefaultGeoreference(world);
-  ACesiumSunSky* sunSkyActor = world->SpawnActor<ACesiumSunSky>();
-  ACesium3DTileset* tilesetActor = world->SpawnActor<ACesium3DTileset>();
-  APlayerStart* playerStartActor = world->SpawnActor<APlayerStart>();
-
-  FSoftObjectPath objectPath(
-      TEXT("Class'/CesiumForUnreal/DynamicPawn.DynamicPawn_C'"));
-  TSoftObjectPtr<UObject> DynamicPawn = TSoftObjectPtr<UObject>(objectPath);
-  AGlobeAwareDefaultPawn* pawnActor = world->SpawnActor<AGlobeAwareDefaultPawn>(
-      Cast<UClass>(DynamicPawn.LoadSynchronous()));
-
-  TestNotNull("cameraManager is valid", cameraManager);
-  TestNotNull("georeference is valid", georeference);
-  TestNotNull("sunSkyActor is valid", sunSkyActor);
-  TestNotNull("tilesetActor is valid", tilesetActor);
-  TestNotNull("playerStartActor is valid", playerStartActor);
-  TestNotNull("pawnActor is valid", pawnActor);
-
   // Configure similar to Google Tiles sample
-  FVector targetOrigin(-122.083969, 37.424492, 142.859116);
-  FString targetUrl(
-      "https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyCnRPXWDIj1LuX6OWIweIqZFHHoXVgdYss");
+  // setupForGoogleTiles(world);
+  setupForDenver(world);
 
-  georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(targetOrigin);
-  tilesetActor->SetUrl(targetUrl);
-  tilesetActor->SetTilesetSource(ETilesetSource::FromUrl);
-  pawnActor->SetActorLocation(FVector(0, 0, 0));
-  pawnActor->SetActorRotation(FRotator(-25, 95, 0));
-  pawnActor->AutoPossessPlayer = EAutoReceiveInput::Player0;
+  world->BeginPlay();
 
+  // Spin for 5 seconds, letting our game objects tick
+  const double testMaxTime = 5.0;
+  tickWorld(world, testMaxTime);
+
+  // Cleanup
 #if CREATE_TEST_IN_EDITOR_WORLD
   // Let all objects persist
 #else
