e0deda6d0296d0f48a73728c439e8df4cdde3954
Brian L <130494071+csciguy8@users.noreply.github.com>
2023-08-25 15:32:23 -0600

    Refactor common vars into context class. Add structure to loop until tiles loaded (still WIP)

--- Source/CesiumRuntime/Private/Tests/CesiumLoadTest.cpp
+++ Source/CesiumRuntime/Private/Tests/CesiumLoadTest.cpp
@@ -8,6 +8,7 @@
 
 #include "Editor.h"
 #include "Engine/World.h"
+#include "EngineAnalytics.h"
 #include "EngineUtils.h"
 #include "GameFramework/PlayerStart.h"
 
@@ -31,7 +32,29 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(
     "Cesium.LoadTest",
     EAutomationTestFlags::EditorContext | EAutomationTestFlags::PerfFilter)
 
-void tickWorld(UWorld* world, double time) {
+struct LoadTestContext {
+  UWorld* world;
+  ACesiumGeoreference* georeference;
+  AGlobeAwareDefaultPawn* pawn;
+  std::vector<ACesium3DTileset*> tilesets;
+};
+
+bool neverBreak(LoadTestContext& context) { return false; }
+
+bool breakWhenTilesetsLoaded(LoadTestContext& context) {
+  std::vector<ACesium3DTileset*>::const_iterator it;
+  for (it = context.tilesets.begin(); it != context.tilesets.end(); ++it) {
+    ACesium3DTileset* tileset = *it;
+    if (tileset->GetLoadProgress() != 100)
+      return false;
+  }
+  return true;
+}
+
+bool tickWorldUntil(
+    LoadTestContext& context,
+    double time,
+    std::function<bool(LoadTestContext&)> breakFunction) {
   const double minStepTime = 0.001; // Don't loop faster than 100 fps
 
   const double testStartMark = FPlatformTime::Seconds();
@@ -42,7 +65,10 @@ void tickWorld(UWorld* world, double time) {
     double frameTimeMark = FPlatformTime::Seconds();
 
     if (frameTimeMark > testEndMark)
-      break;
+      return true;
+
+    if (breakFunction(context))
+      return false;
 
     double frameElapsedTime = frameTimeMark - lastTimeMark;
 
@@ -53,7 +79,7 @@ void tickWorld(UWorld* world, double time) {
     }
 
     // Let world tick at same rate as this loop
-    world->Tick(ELevelTick::LEVELTICK_All, frameElapsedTime);
+    context.world->Tick(ELevelTick::LEVELTICK_All, frameElapsedTime);
 
     // Derived from TimerManagerTests.cpp, TimerTest_TickWorld
     GFrameCounter++;
@@ -62,46 +88,41 @@ void tickWorld(UWorld* world, double time) {
   };
 }
 
-void setupForGoogleTiles(
-    UWorld* world,
-    ACesiumGeoreference* georeference,
-    AGlobeAwareDefaultPawn* pawn,
-    std::vector<ACesium3DTileset*>& createdTilesets) {
+void setupForGoogleTiles(LoadTestContext& context) {
 
   FVector targetOrigin(-122.083969, 37.424492, 142.859116);
   FString targetUrl(
       "https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyCnRPXWDIj1LuX6OWIweIqZFHHoXVgdYss");
 
-  georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(targetOrigin);
+  context.georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(
+      targetOrigin);
 
-  pawn->SetActorLocation(FVector(0, 0, 0));
-  pawn->SetActorRotation(FRotator(-25, 95, 0));
+  context.pawn->SetActorLocation(FVector(0, 0, 0));
+  context.pawn->SetActorRotation(FRotator(-25, 95, 0));
 
-  ACesium3DTileset* tileset = world->SpawnActor<ACesium3DTileset>();
+  ACesium3DTileset* tileset = context.world->SpawnActor<ACesium3DTileset>();
   tileset->SetUrl(targetUrl);
   tileset->SetTilesetSource(ETilesetSource::FromUrl);
   tileset->SetActorLabel(TEXT("Google Photorealistic 3D Tiles"));
 
-  createdTilesets.push_back(tileset);
+  context.tilesets.push_back(tileset);
 }
 
-void setupForDenver(
-    UWorld* world,
-    ACesiumGeoreference* georeference,
-    AGlobeAwareDefaultPawn* pawn,
-    std::vector<ACesium3DTileset*>& createdTilesets) {
+void setupForDenver(LoadTestContext& context) {
 
   FVector targetOrigin(-104.988892, 39.743462, 1798.679443);
   FString ionToken(
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NmZhZTk4NS01MDFmLTRjODgtOTlkYy04NjIwODhiZWExOGYiLCJpZCI6MjU5LCJpYXQiOjE2ODg1MTI4ODd9.haoe5hsJyfHk1dQAHVK6N8dW_kfmtdbyuhlGwFdEHbM");
 
-  georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(targetOrigin);
+  context.georeference->SetGeoreferenceOriginLongitudeLatitudeHeight(
+      targetOrigin);
 
-  pawn->SetActorLocation(FVector(0, 0, 0));
-  pawn->SetActorRotation(FRotator(-5.2, -149.4, 0));
+  context.pawn->SetActorLocation(FVector(0, 0, 0));
+  context.pawn->SetActorRotation(FRotator(-5.2, -149.4, 0));
 
   // Add Cesium World Terrain
-  ACesium3DTileset* worldTerrainTileset = world->SpawnActor<ACesium3DTileset>();
+  ACesium3DTileset* worldTerrainTileset =
+      context.world->SpawnActor<ACesium3DTileset>();
   worldTerrainTileset->SetTilesetSource(ETilesetSource::FromCesiumIon);
   worldTerrainTileset->SetIonAssetID(1);
   worldTerrainTileset->SetIonAccessToken(ionToken);
@@ -119,15 +140,44 @@ void setupForDenver(
   worldTerrainTileset->AddInstanceComponent(pOverlay);
 
   // Aerometrex Denver
-  ACesium3DTileset* aerometrexTileset = world->SpawnActor<ACesium3DTileset>();
+  ACesium3DTileset* aerometrexTileset =
+      context.world->SpawnActor<ACesium3DTileset>();
   aerometrexTileset->SetTilesetSource(ETilesetSource::FromCesiumIon);
   aerometrexTileset->SetIonAssetID(354307);
   aerometrexTileset->SetIonAccessToken(ionToken);
   aerometrexTileset->SetMaximumScreenSpaceError(2.0);
   aerometrexTileset->SetActorLabel(TEXT("Aerometrex Denver"));
 
-  createdTilesets.push_back(worldTerrainTileset);
-  createdTilesets.push_back(aerometrexTileset);
+  context.tilesets.push_back(worldTerrainTileset);
+  context.tilesets.push_back(aerometrexTileset);
+}
+
+void createCommonWorldObjects(LoadTestContext& context) {
+
+#if CREATE_TEST_IN_EDITOR_WORLD
+  context.world = FAutomationEditorCommonUtils::CreateNewMap();
+#else
+  context.world = UWorld::CreateWorld(EWorldType::Game, false);
+  FWorldContext& worldContext =
+      GEngine->CreateNewWorldContext(EWorldType::Game);
+  worldContext.SetCurrentWorld(context.world);
+#endif
+
+  ACesiumCameraManager* cameraManager =
+      ACesiumCameraManager::GetDefaultCameraManager(context.world);
+  ACesiumSunSky* sunSky = context.world->SpawnActor<ACesiumSunSky>();
+  APlayerStart* playerStart = context.world->SpawnActor<APlayerStart>();
+
+  FSoftObjectPath objectPath(
+      TEXT("Class'/CesiumForUnreal/DynamicPawn.DynamicPawn_C'"));
+  TSoftObjectPtr<UObject> DynamicPawn = TSoftObjectPtr<UObject>(objectPath);
+
+  context.georeference =
+      ACesiumGeoreference::GetDefaultGeoreference(context.world);
+  context.pawn = context.world->SpawnActor<AGlobeAwareDefaultPawn>(
+      Cast<UClass>(DynamicPawn.LoadSynchronous()));
+
+  context.pawn->AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 bool FCesiumLoadTest::RunTest(const FString& Parameters) {
@@ -135,40 +185,17 @@ bool FCesiumLoadTest::RunTest(const FString& Parameters) {
   //
   // Programmatically set up the world
   //
-
-#if CREATE_TEST_IN_EDITOR_WORLD
-  UWorld* world = FAutomationEditorCommonUtils::CreateNewMap();
-#else
-  UWorld* world = UWorld::CreateWorld(EWorldType::Game, false);
-  FWorldContext& context = GEngine->CreateNewWorldContext(EWorldType::Game);
-  context.SetCurrentWorld(world);
-#endif
-  TestNotNull("world is valid", world);
-
-  // Create common objects across all locations
-  ACesiumCameraManager* cameraManager =
-      ACesiumCameraManager::GetDefaultCameraManager(world);
-  ACesiumGeoreference* georeference =
-      ACesiumGeoreference::GetDefaultGeoreference(world);
-  ACesiumSunSky* sunSky = world->SpawnActor<ACesiumSunSky>();
-  APlayerStart* playerStart = world->SpawnActor<APlayerStart>();
-
-  FSoftObjectPath objectPath(
-      TEXT("Class'/CesiumForUnreal/DynamicPawn.DynamicPawn_C'"));
-  TSoftObjectPtr<UObject> DynamicPawn = TSoftObjectPtr<UObject>(objectPath);
-  AGlobeAwareDefaultPawn* pawn = world->SpawnActor<AGlobeAwareDefaultPawn>(
-      Cast<UClass>(DynamicPawn.LoadSynchronous()));
-  pawn->AutoPossessPlayer = EAutoReceiveInput::Player0;
+  LoadTestContext context;
+  createCommonWorldObjects(context);
 
   // Configure location specific objects
-  std::vector<ACesium3DTileset*> tilesets;
   const size_t locationIndex = 1;
   switch (locationIndex) {
   case 0:
-    setupForGoogleTiles(world, georeference, pawn, tilesets);
+    setupForGoogleTiles(context);
     break;
   case 1:
-    setupForDenver(world, georeference, pawn, tilesets);
+    setupForDenver(context);
     break;
   default:
     break;
@@ -176,31 +203,45 @@ bool FCesiumLoadTest::RunTest(const FString& Parameters) {
 
   // Halt tileset updates and reset them
   std::vector<ACesium3DTileset*>::iterator it;
-  for (it = tilesets.begin(); it != tilesets.end(); ++it) {
+  for (it = context.tilesets.begin(); it != context.tilesets.end(); ++it) {
     ACesium3DTileset* tileset = *it;
     tileset->SuspendUpdate = true;
     tileset->RefreshTileset();
   }
 
+  // Let world settle for 1 second
+  tickWorldUntil(context, 1.0, neverBreak);
+
   // Turn updates back on
-  for (it = tilesets.begin(); it != tilesets.end(); ++it) {
+  for (it = context.tilesets.begin(); it != context.tilesets.end(); ++it) {
     ACesium3DTileset* tileset = *it;
     tileset->SuspendUpdate = false;
   }
 
-  // Spin for 5 seconds, letting our game objects tick
-  const double testMaxTime = 5.0;
-  tickWorld(world, testMaxTime);
+  // Spin for a maximum of 5 seconds, or until tilesets finish loading
+  bool timedOut = tickWorldUntil(context, 5.0, breakWhenTilesetsLoaded);
+
+  if (FEngineAnalytics::IsAvailable()) {
+    if (timedOut) {
+      FEngineAnalytics::GetProvider().RecordEvent(
+          TEXT("LoadTest.TimeoutEvent"));
+      UE_LOG(LogCesium, Error, TEXT("Test timed out"));
+    } else {
+      FEngineAnalytics::GetProvider().RecordEvent(
+          TEXT("LoadTest.CompletionEvent"));
+      UE_LOG(LogCesium, Display, TEXT("Test completed"));
+    }
+  }
 
   // Cleanup
 #if CREATE_TEST_IN_EDITOR_WORLD
   // Let all objects persist
 #else
-  GEngine->DestroyWorldContext(world);
-  world->DestroyWorld(false);
+  GEngine->DestroyWorldContext(context.world);
+  context.world->DestroyWorld(false);
 #endif
 
-  return true;
+  return !timedOut;
 }
 
 #endif
