67a5c6a356678546284e43bfa2775af809c8250e
Kevin Ring <kevin@kotachrome.com>
2023-09-14 18:21:08 +1000

    Remove tileset's custom ApplyWorldOffset.

--- Source/CesiumRuntime/Private/Cesium3DTilesetRoot.cpp
+++ Source/CesiumRuntime/Private/Cesium3DTilesetRoot.cpp
@@ -8,25 +8,8 @@
 #include "VecMath.h"
 
 UCesium3DTilesetRoot::UCesium3DTilesetRoot()
-    : _worldOriginLocation(0.0),
-      _absoluteLocation(0.0, 0.0, 0.0),
-      _tilesetToUnrealRelativeWorld(1.0) {
+    : _absoluteLocation(0.0, 0.0, 0.0), _tilesetToUnrealRelativeWorld(1.0) {
   PrimaryComponentTick.bCanEverTick = false;
-}
-
-void UCesium3DTilesetRoot::ApplyWorldOffset(
-    const FVector& InOffset,
-    bool bWorldShift) {
-  USceneComponent::ApplyWorldOffset(InOffset, bWorldShift);
-
-  const FIntVector& oldOrigin = this->GetWorld()->OriginLocation;
-  this->_worldOriginLocation = VecMath::subtract3D(oldOrigin, InOffset);
-
-  // Do _not_ call _updateAbsoluteLocation. The absolute position doesn't change
-  // with an origin rebase, and we'll lose precision if we update the absolute
-  // location here.
-
-  this->_updateTilesetToUnrealRelativeWorldTransform();
 }
 
 void UCesium3DTilesetRoot::HandleGeoreferenceUpdated() {
@@ -74,9 +57,7 @@ bool UCesium3DTilesetRoot::MoveComponentImpl(
 
 void UCesium3DTilesetRoot::_updateAbsoluteLocation() {
   const FVector& newLocation = this->GetRelativeLocation();
-  const FIntVector& originLocation = this->GetWorld()->OriginLocation;
-  this->_absoluteLocation = VecMath::add3D(originLocation, newLocation);
-  this->_worldOriginLocation = VecMath::createVector3D(originLocation);
+  this->_absoluteLocation = VecMath::createVector3D(newLocation);
 }
 
 void UCesium3DTilesetRoot::_updateTilesetToUnrealRelativeWorldTransform() {
@@ -87,8 +68,7 @@ void UCesium3DTilesetRoot::_updateTilesetToUnrealRelativeWorldTransform() {
           ->GetGeoTransforms()
           .GetEllipsoidCenteredToAbsoluteUnrealWorldTransform();
 
-  glm::dvec3 relativeLocation =
-      this->_absoluteLocation - this->_worldOriginLocation;
+  glm::dvec3 relativeLocation = this->_absoluteLocation;
 
   FMatrix tilesetActorToUeLocal =
       this->GetRelativeTransform().ToMatrixWithScale();
