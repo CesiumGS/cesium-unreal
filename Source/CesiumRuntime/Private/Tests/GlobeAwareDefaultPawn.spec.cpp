7ca6ada3e486d36353ec3da5dd2dab24c2317e36
Ashley Rogers <ashley.rogers@cesium.com>
2024-01-30 11:22:12 -0500

    More tests for AGlobeAwareDefaultPawn

--- Source/CesiumRuntime/Private/Tests/GlobeAwareDefaultPawn.spec.cpp
+++ Source/CesiumRuntime/Private/Tests/GlobeAwareDefaultPawn.spec.cpp
@@ -2,13 +2,15 @@
 
 #if WITH_EDITOR
 
+#include "GlobeAwareDefaultPawn.h"
 #include "CesiumFlyToComponent.h"
+#include "CesiumGeoreference.h"
 #include "CesiumGlobeAnchorComponent.h"
 #include "CesiumTestHelpers.h"
+#include "CesiumWgs84Ellipsoid.h"
 #include "Editor.h"
 #include "Engine/World.h"
 #include "EngineUtils.h"
-#include "GlobeAwareDefaultPawn.h"
 #include "Misc/AutomationTest.h"
 #include "Tests/AutomationEditorCommon.h"
 
@@ -23,6 +25,12 @@ FDelegateHandle subscriptionPostPIEStarted;
 END_DEFINE_SPEC(FGlobeAwareDefaultPawnSpec)
 
 void FGlobeAwareDefaultPawnSpec::Define() {
+  const FVector philadelphiaEcef =
+      FVector(1253264.69280105, -4732469.91065521, 4075112.40412297);
+
+  const FVector tokyoEcef =
+      FVector(-3960158.65587452, 3352568.87555906, 3697235.23506459);
+
   Describe(
       "should not spike altitude when very close to final destination",
       [this]() {
@@ -89,6 +97,194 @@ void FGlobeAwareDefaultPawnSpec::Define() {
 
           pPawn->Destroy();
         });
+      });
+
+  Describe(
+      "should interpolate between positions and rotations correctly",
+      [this, tokyoEcef, philadelphiaEcef]() {
+        LatentBeforeEach(
+            EAsyncExecution::TaskGraphMainThread,
+            [this](const FDoneDelegate& done) {
+              UWorld* pWorld = FAutomationEditorCommonUtils::CreateNewMap();
+
+              AGlobeAwareDefaultPawn* pPawn =
+                  pWorld->SpawnActor<AGlobeAwareDefaultPawn>();
+              UCesiumFlyToComponent* pFlyTo =
+                  Cast<UCesiumFlyToComponent>(pPawn->AddComponentByClass(
+                      UCesiumFlyToComponent::StaticClass(),
+                      false,
+                      FTransform::Identity,
+                      false));
+              pFlyTo->RotationToUse =
+                  ECesiumFlyToRotation::ControlRotationInEastSouthUp;
+
+              subscriptionPostPIEStarted =
+                  FEditorDelegates::PostPIEStarted.AddLambda(
+                      [done](bool isSimulating) { done.Execute(); });
+              FRequestPlaySessionParams params{};
+              GEditor->RequestPlaySession(params);
+            });
+        BeforeEach(EAsyncExecution::TaskGraphMainThread, [this]() {
+          FEditorDelegates::PostPIEStarted.Remove(subscriptionPostPIEStarted);
+        });
+        AfterEach(EAsyncExecution::TaskGraphMainThread, [this]() {
+          GEditor->RequestEndPlayMap();
+        });
+        It("should match the beginning and ending points of the fly-to",
+           [this]() {
+             UWorld* pWorld = GEditor->PlayWorld;
+
+             TActorIterator<AGlobeAwareDefaultPawn> it(pWorld);
+             AGlobeAwareDefaultPawn* pPawn = *it;
+             UCesiumFlyToComponent* pFlyTo =
+                 pPawn->FindComponentByClass<UCesiumFlyToComponent>();
+             TestNotNull("pFlyTo", pFlyTo);
+             pFlyTo->Duration = 5.0f;
+
+             UCesiumGlobeAnchorComponent* pGlobeAnchor =
+                 pPawn->FindComponentByClass<UCesiumGlobeAnchorComponent>();
+             TestNotNull("pGlobeAnchor", pGlobeAnchor);
+
+             pGlobeAnchor->MoveToLongitudeLatitudeHeight(
+                 FVector(25.0, 10.0, 100.0));
+
+             // Start flying somewhere else
+             pFlyTo->FlyToLocationLongitudeLatitudeHeight(
+                 FVector(25.0, 25.0, 100.0),
+                 0.0,
+                 0.0,
+                 false);
+
+             TestEqual(
+                 "Location is the same as the start point",
+                 pGlobeAnchor->GetLongitudeLatitudeHeight(),
+                 FVector(25.0, 10.0, 100.0));
+
+             // Tick to the end
+             Cast<UActorComponent>(pFlyTo)->TickComponent(
+                 5.0f,
+                 ELevelTick::LEVELTICK_All,
+                 nullptr);
+
+             TestEqual(
+                 "Location is the same as the end point",
+                 pGlobeAnchor->GetLongitudeLatitudeHeight(),
+                 FVector(25.0, 25.0, 100.0));
+
+             pPawn->Destroy();
+           });
+        It("should correctly compute the midpoint of the flight",
+           [this, tokyoEcef, philadelphiaEcef]() {
+             UWorld* pWorld = GEditor->PlayWorld;
+
+             TActorIterator<AGlobeAwareDefaultPawn> it(pWorld);
+             AGlobeAwareDefaultPawn* pPawn = *it;
+             UCesiumFlyToComponent* pFlyTo =
+                 pPawn->FindComponentByClass<UCesiumFlyToComponent>();
+             TestNotNull("pFlyTo", pFlyTo);
+             pFlyTo->Duration = 5.0f;
+             pFlyTo->HeightPercentageCurve = nullptr;
+             pFlyTo->MaximumHeightByDistanceCurve = nullptr;
+             pFlyTo->ProgressCurve = nullptr;
+
+             UCesiumGlobeAnchorComponent* pGlobeAnchor =
+                 pPawn->FindComponentByClass<UCesiumGlobeAnchorComponent>();
+             TestNotNull("pGlobeAnchor", pGlobeAnchor);
+
+             pGlobeAnchor->MoveToEarthCenteredEarthFixedPosition(
+                 philadelphiaEcef);
+             pFlyTo->FlyToLocationEarthCenteredEarthFixed(tokyoEcef, 0, 0, 0);
+
+             // Tick half way through
+             Cast<UActorComponent>(pFlyTo)->TickComponent(
+                 2.5f,
+                 ELevelTick::LEVELTICK_All,
+                 nullptr);
+
+             FVector expectedResult = FVector(
+                 -2062499.3622640674,
+                 -1052346.4221710551,
+                 5923430.4378960524);
+
+             TestEqual(
+                 "Midpoint location is correct",
+                 pGlobeAnchor->GetEarthCenteredEarthFixedPosition(),
+                 expectedResult);
+
+             pPawn->Destroy();
+           });
+        It("should match the start and end rotations",
+           [this, tokyoEcef, philadelphiaEcef]() {
+             UWorld* pWorld = GEditor->PlayWorld;
+
+             TActorIterator<AGlobeAwareDefaultPawn> it(pWorld);
+             AGlobeAwareDefaultPawn* pPawn = *it;
+             UCesiumFlyToComponent* pFlyTo =
+                 pPawn->FindComponentByClass<UCesiumFlyToComponent>();
+             TestNotNull("pFlyTo", pFlyTo);
+             pFlyTo->Duration = 5.0f;
+             pFlyTo->HeightPercentageCurve = nullptr;
+             pFlyTo->MaximumHeightByDistanceCurve = nullptr;
+             pFlyTo->ProgressCurve = nullptr;
+
+             UCesiumGlobeAnchorComponent* pGlobeAnchor =
+                 pPawn->FindComponentByClass<UCesiumGlobeAnchorComponent>();
+             TestNotNull("pGlobeAnchor", pGlobeAnchor);
+
+             FQuat sourceRotation = FRotator(0, 0, 0).Quaternion();
+             FQuat targetRotation = FRotator(45, 180, 0).Quaternion();
+             FQuat midpointRotation =
+                 FQuat::Slerp(sourceRotation, targetRotation, 0.5);
+
+             pGlobeAnchor->MoveToEarthCenteredEarthFixedPosition(
+                 philadelphiaEcef);
+             pGlobeAnchor->SetEastSouthUpRotation(sourceRotation);
+             pFlyTo->FlyToLocationEarthCenteredEarthFixed(
+                 tokyoEcef,
+                 180,
+                 45,
+                 false);
+
+             TestTrue(
+                 "Start rotation is correct",
+                 pPawn->Controller->GetControlRotation().Quaternion().Equals(
+                     sourceRotation,
+                     CesiumUtility::Math::Epsilon4));
+
+             // Tick half way through
+             Cast<UActorComponent>(pFlyTo)->TickComponent(
+                 2.5f,
+                 ELevelTick::LEVELTICK_All,
+                 nullptr);
+
+             TestTrue(
+                 "Midpoint rotation is correct",
+                 pPawn->Controller->GetControlRotation().Quaternion().Equals(
+                     midpointRotation,
+                     CesiumUtility::Math::Epsilon4));
+
+             FVector currentEastSouthRotationEuler =
+                 pGlobeAnchor->GetEastSouthUpRotation().Euler();
+             FVector midpointEuler = midpointRotation.Euler();
+
+             // Tick to the end
+             Cast<UActorComponent>(pFlyTo)->TickComponent(
+                 2.5f,
+                 ELevelTick::LEVELTICK_All,
+                 nullptr);
+
+             TestTrue(
+                 "End location is correct",
+                 pPawn->Controller->GetControlRotation().Quaternion().Equals(
+                     targetRotation,
+                     CesiumUtility::Math::Epsilon4));
+
+             FVector endEastSouthRotationEuler =
+                 pGlobeAnchor->GetEastSouthUpRotation().Euler();
+             FVector endpointEuler = targetRotation.Euler();
+
+             pPawn->Destroy();
+           });
       });
 }
 
