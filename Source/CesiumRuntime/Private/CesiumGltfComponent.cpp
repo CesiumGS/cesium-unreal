349023dbbb507501e21d271295533d791d92075e
Ashley Rogers <ashley.rogers@cesium.com>
2024-04-17 16:00:19 -0400

    Revert "Update name of Transparent texture"
    This reverts commit 858110ccf8e4d8c3e256f6d2a163427cf5a910a4.

--- Source/CesiumRuntime/Private/CesiumGltfComponent.cpp
+++ Source/CesiumRuntime/Private/CesiumGltfComponent.cpp
@@ -3330,7 +3330,7 @@ UCesiumGltfComponent::UCesiumGltfComponent() : USceneComponent() {
     ConstructorHelpers::FObjectFinder<UMaterialInstance>
         BaseMaterialWithTranslucency;
     ConstructorHelpers::FObjectFinder<UMaterialInstance> BaseMaterialWithWater;
-    ConstructorHelpers::FObjectFinder<UTexture2D> TransparentTiny;
+    ConstructorHelpers::FObjectFinder<UTexture2D> Transparent1x1;
     FConstructorStatics()
         : BaseMaterial(TEXT(
               "/CesiumForUnreal/Materials/Instances/MI_CesiumThreeOverlaysAndClipping.MI_CesiumThreeOverlaysAndClipping")),
@@ -3338,8 +3338,8 @@ UCesiumGltfComponent::UCesiumGltfComponent() : USceneComponent() {
               "/CesiumForUnreal/Materials/Instances/MI_CesiumThreeOverlaysAndClippingTranslucent.MI_CesiumThreeOverlaysAndClippingTranslucent")),
           BaseMaterialWithWater(TEXT(
               "/CesiumForUnreal/Materials/Instances/MI_CesiumThreeOverlaysAndClippingAndWater.MI_CesiumThreeOverlaysAndClippingAndWater")),
-          TransparentTiny(
-              TEXT("/CesiumForUnreal/Textures/TransparentTiny.TransparentTiny")) {
+          Transparent1x1(
+              TEXT("/CesiumForUnreal/Textures/transparent1x1.transparent1x1")) {
     }
   };
   static FConstructorStatics ConstructorStatics;
@@ -3348,7 +3348,7 @@ UCesiumGltfComponent::UCesiumGltfComponent() : USceneComponent() {
   this->BaseMaterialWithTranslucency =
       ConstructorStatics.BaseMaterialWithTranslucency.Object;
   this->BaseMaterialWithWater = ConstructorStatics.BaseMaterialWithWater.Object;
-  this->TransparentTiny = ConstructorStatics.TransparentTiny.Object;
+  this->Transparent1x1 = ConstructorStatics.Transparent1x1.Object;
 
   PrimaryComponentTick.bCanEverTick = false;
 }
@@ -3495,12 +3495,12 @@ void UCesiumGltfComponent::DetachRasterTile(
                     "Texture",
                     EMaterialParameterAssociation::LayerParameter,
                     i),
-                this->TransparentTiny);
+                this->Transparent1x1);
           }
         } else {
           pMaterial->SetTextureParameterValue(
               createSafeName(rasterTile.getOverlay().getName(), "_Texture"),
-              this->TransparentTiny);
+              this->Transparent1x1);
         }
       });
 }
