29b59f4c4703c1b8b329f7880e81a91b70492322
Kevin Ring <kevin@kotachrome.com>
2024-06-11 20:55:30 +1000

    Proper overlay port for Unreal OpenSSL.

--- extern/unreal-linux-toolchain.cmake
+++ extern/unreal-linux-toolchain.cmake
@@ -19,31 +19,3 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
-
-SET(UNREAL_ROOT_DIR_RELATIVE "$ENV{UNREAL_ENGINE_LIBCXX_DIR}/../../../../../")
-FILE(REAL_PATH "${UNREAL_ROOT_DIR_RELATIVE}" UNREAL_ROOT_DIR)
-
-# Some old versions of UE used to have multiple versions of OpenSSL, with different versions used on different platforms.
-# That is no longer the case in UE 5.2, 5.3, and 5.4, but the below logic may need to change if that ever happens again.
-SET(OPENSSL_VERSIONS_DIR "${UNREAL_ROOT_DIR}/Engine/Source/ThirdParty/OpenSSL")
-FILE(GLOB OPENSSL_POSSIBLE_ROOT_DIRS LIST_DIRECTORIES true "${OPENSSL_VERSIONS_DIR}/*")
-
-# Find the subdirectories, each of which should represent an OpenSSL version.
-SET(OPENSSL_VERSION_DIRS "")
-FOREACH(OPENSSL_POSSIBLE_ROOT_DIR IN LISTS OPENSSL_POSSIBLE_ROOT_DIRS)
-  IF(IS_DIRECTORY "${OPENSSL_POSSIBLE_ROOT_DIR}")
-    LIST(APPEND OPENSSL_VERSION_DIRS "${OPENSSL_POSSIBLE_ROOT_DIR}")
-  ENDIF()
-ENDFOREACH()
-
-# There should be exactly one version, otherwise we don't know what to do.
-LIST(LENGTH OPENSSL_VERSION_DIRS OPENSSL_VERSION_DIRS_LENGTH)
-IF(NOT OPENSSL_VERSION_DIRS_LENGTH EQUAL 1)
-  MESSAGE(FATAL_ERROR "Could not deduce the OpenSSL root directory because there is not exactly one directory entry matching `${OPENSSL_VERSIONS_DIR}/1.1.1?`.")
-ENDIF()
-
-LIST(GET OPENSSL_VERSION_DIRS 0 OPENSSL_ROOT_DIR)
-
-set(OPENSSL_INCLUDE_DIR "${OPENSSL_ROOT_DIR}/include/Unix/x86_64-unknown-linux-gnu")
-set(OPENSSL_CRYPTO_LIBRARY "${OPENSSL_ROOT_DIR}/lib/Unix/x86_64-unknown-linux-gnu/libcrypto.a")
-set(OPENSSL_SSL_LIBRARY "${OPENSSL_ROOT_DIR}/lib/Unix/x86_64-unknown-linux-gnu/libssl.a")
--- extern/vcpkg-overlays/openssl/portfile.cmake
+++ extern/vcpkg-overlays/openssl/portfile.cmake
@@ -1,1 +1,49 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+message(STATUS "Using Unreal Engine installation at $ENV{UNREAL_ENGINE_ROOT}")
+
+# Some old versions of UE used to have multiple versions of OpenSSL, with different versions used on different platforms.
+# That is no longer the case in UE 5.2, 5.3, and 5.4, but the below logic may need to change if that ever happens again.
+set(OPENSSL_VERSIONS_DIR "$ENV{UNREAL_ENGINE_ROOT}/Engine/Source/ThirdParty/OpenSSL")
+file(GLOB OPENSSL_POSSIBLE_ROOT_DIRS LIST_DIRECTORIES true "${OPENSSL_VERSIONS_DIR}/*")
+
+# Find the subdirectories, each of which should represent an OpenSSL version.
+set(OPENSSL_VERSION_DIRS "")
+foreach(OPENSSL_POSSIBLE_ROOT_DIR IN LISTS OPENSSL_POSSIBLE_ROOT_DIRS)
+  if(IS_DIRECTORY "${OPENSSL_POSSIBLE_ROOT_DIR}")
+    list(APPEND OPENSSL_VERSION_DIRS "${OPENSSL_POSSIBLE_ROOT_DIR}")
+  endif()
+endforeach()
+
+# There should be exactly one version, otherwise we don't know what to do.
+list(LENGTH OPENSSL_VERSION_DIRS OPENSSL_VERSION_DIRS_LENGTH)
+if(NOT OPENSSL_VERSION_DIRS_LENGTH EQUAL 1)
+  message(FATAL_ERROR "Could not deduce the OpenSSL root directory because there is not exactly one directory matching `${OPENSSL_VERSIONS_DIR}/*`.")
+endif()
+
+list(GET OPENSSL_VERSION_DIRS 0 OPENSSL_ROOT_DIR)
+
+if (VCPKG_TARGET_IS_ANDROID)
+elseif (VCPKG_TARGET_IS_IOS)
+elseif (VCPKG_TARGET_IS_OSX)
+elseif (VCPKG_TARGET_IS_WINDOWS)
+elseif (VCPKG_TARGET_IS_LINUX)
+  if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(OPENSSL_LIB_PATH "${OPENSSL_ROOT_DIR}/lib/Unix/x86_64-unknown-linux-gnu")
+    set(OPENSSL_INCLUDE_PATH "${OPENSSL_ROOT_DIR}/include/Unix/x86_64-unknown-linux-gnu")
+  elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+    set(OPENSSL_LIB_PATH "${OPENSSL_ROOT_DIR}/lib/Unix/aarch64-unknown-linux-gnueabi")
+    set(OPENSSL_INCLUDE_PATH "${OPENSSL_ROOT_DIR}/include/Unix/aarch64-unknown-linux-gnueabi")
+  else()
+    message(FATAL_ERROR "Unknown Unreal / OpenSSL paths for Linux platform with architecture ${VCPKG_TARGET_ARCHITECTURE}.")
+  endif()
+else()
+  message(FATAL_ERROR "Unknown Unreal / OpenSSL paths for VCPKG_CMAKE_SYSTEM_NAME ${VCPKG_CMAKE_SYSTEM_NAME}.")
+endif()
+
+file(INSTALL "${OPENSSL_LIB_PATH}/" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+file(INSTALL "${OPENSSL_INCLUDE_PATH}/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/libcrypto.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libcrypto.pc" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/libssl.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libssl.pc" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/openssl.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/openssl.pc" @ONLY)
