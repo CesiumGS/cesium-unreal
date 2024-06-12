bf6da8e42d1690963268989e19957d1f0a41e0d0
Kevin Ring <kevin@kotachrome.com>
2024-06-13 09:54:11 +1000

    Don't let ktx be a framework on iOS.

--- extern/vcpkg-overlays/ktx/portfile.cmake
+++ extern/vcpkg-overlays/ktx/portfile.cmake
@@ -11,6 +11,7 @@ vcpkg_from_github(
         0004-quirks.patch
         0005-no-vendored-libs.patch
         CESIUM-0001-do-not-define-_DEBUG.patch
+        CESIUM-0002-not-a-framework.patch
 )
 file(REMOVE "${SOURCE_PATH}/other_include/zstd_errors.h")
 
