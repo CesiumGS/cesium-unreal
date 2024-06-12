139777d16fc731fa2297f81c71a87f16072e5bce
Kevin Ring <kevin@kotachrome.com>
2024-06-12 12:30:17 +1000

    VCPKG_SYSTEM_NAME -> VCPKG_CMAKE_SYSTEM_NAME

--- extern/vcpkg-overlays/triplets/osx-common.cmake
+++ extern/vcpkg-overlays/triplets/osx-common.cmake
@@ -1,5 +1,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/common.cmake")
 
-set(VCPKG_SYSTEM_NAME Darwin)
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
