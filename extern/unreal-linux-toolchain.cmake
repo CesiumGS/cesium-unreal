fd1625f28693541309358f355176f1b8ec0e1e59
Kevin Ring <kevin@kotachrome.com>
2024-06-14 07:35:50 +1000

    Add visibility-related flags to triplets.
    To hopefully fix large number of linker warnings on macOS.

--- extern/unreal-linux-toolchain.cmake
+++ extern/unreal-linux-toolchain.cmake
@@ -10,7 +10,8 @@ SET(CMAKE_BUILD_WITH_INSTALL_RPATH on)
 SET(CMAKE_POSITION_INDEPENDENT_CODE on)
 
 # These were deduced by scouring Unreal's LinuxToolChain.cs.
-SET(CMAKE_CXX_FLAGS "-nostdinc++ -I$ENV{UNREAL_ENGINE_LIBCXX_DIR}/include -I$ENV{UNREAL_ENGINE_LIBCXX_DIR}/include/c++/v1 -target x86_64-unknown-linux-gnu")
+SET(CMAKE_C_FLAGS "-fvisibility-ms-compat -fvisibility-inlines-hidden")
+SET(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -nostdinc++ -I$ENV{UNREAL_ENGINE_LIBCXX_DIR}/include -I$ENV{UNREAL_ENGINE_LIBCXX_DIR}/include/c++/v1 -target x86_64-unknown-linux-gnu")
 SET(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -target x86_64-unknown-linux-gnu --sysroot=$ENV{UNREAL_ENGINE_COMPILER_DIR} -B$ENV{UNREAL_ENGINE_COMPILER_DIR}/usr/lib -B$ENV{UNREAL_ENGINE_COMPILER_DIR}/usr/lib64 -L$ENV{UNREAL_ENGINE_COMPILER_DIR}/usr/lib -L$ENV{UNREAL_ENGINE_COMPILER_DIR}/usr/lib64 -nodefaultlibs -L$ENV{UNREAL_ENGINE_LIBCXX_DIR}/lib/Unix/x86_64-unknown-linux-gnu/ $ENV{UNREAL_ENGINE_LIBCXX_DIR}/lib/Unix/x86_64-unknown-linux-gnu/libc++.a $ENV{UNREAL_ENGINE_LIBCXX_DIR}/lib/Unix/x86_64-unknown-linux-gnu/libc++abi.a -lm -lc -lpthread -lgcc_s -lgcc")
 
 # search for programs in the build host directories
--- extern/vcpkg-overlays/triplets/x64-windows-unreal.cmake
+++ extern/vcpkg-overlays/triplets/x64-windows-unreal.cmake
@@ -3,6 +3,17 @@ include("${CMAKE_CURRENT_LIST_DIR}/shared/common.cmake")
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CXX_FLAGS "/MD /Zp8")
-set(VCPKG_C_FLAGS "/MD /Zp8")
 set(VCPKG_ENV_PASSTHROUGH "UNREAL_ENGINE_ROOT")
+
+# Unreal Engine adds /Zp8 in 64-bit Windows builds to align structs to 8 bytes instead of the
+# default of 16 bytes. There's this nice note in the documentation for that option:
+#   Don't change the setting from the default when you include the Windows SDK headers, either
+#   by using /Zp on the command line or by using #pragma pack. Otherwise, your application may
+#   cause memory corruption at runtime.
+# (https://docs.microsoft.com/en-us/cpp/build/reference/zp-struct-member-alignment?view=msvc-160)
+# Yeah that's not just the Windows SDK, but anything that passes structs across the boundary
+# between compilation units using different versions of that flag. We compile cesium-native
+# with this same option to avoid super-dodgy and hard to debug issues.
+# https://github.com/EpicGames/UnrealEngine/blob/5.2.1-release/Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
+set(VCPKG_CXX_FLAGS "/MD /Zp8")
+set(VCPKG_C_FLAGS "${VCPKG_CXX_FLAGS}")
