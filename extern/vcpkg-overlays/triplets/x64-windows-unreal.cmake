e238e1c8b47155ffd27369e19409143267ac051d
Kevin Ring <kevin@kotachrome.com>
2024-11-11 18:09:32 +1100

    Fix problems building vcpkg Debug libs for UE.

--- extern/vcpkg-overlays/triplets/x64-windows-unreal.cmake
+++ extern/vcpkg-overlays/triplets/x64-windows-unreal.cmake
@@ -4,6 +4,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_ENV_PASSTHROUGH "UNREAL_ENGINE_ROOT")
+set(VCPKG_POLICY_ONLY_RELEASE_CRT enabled)
 
 # Unreal Engine adds /Zp8 in 64-bit Windows builds to align structs to 8 bytes instead of the
 # default of 16 bytes. There's this nice note in the documentation for that option:
@@ -15,8 +16,16 @@ set(VCPKG_ENV_PASSTHROUGH "UNREAL_ENGINE_ROOT")
 # between compilation units using different versions of that flag. We compile cesium-native
 # with this same option to avoid super-dodgy and hard to debug issues.
 # https://github.com/EpicGames/UnrealEngine/blob/5.2.1-release/Engine/Source/Programs/UnrealBuildTool/Platform/Windows/VCToolChain.cs
-set(VCPKG_CXX_FLAGS "/MD /Zp8")
+set(VCPKG_CXX_FLAGS "/Zp8")
 set(VCPKG_C_FLAGS "${VCPKG_CXX_FLAGS}")
+
+# Use an unreasonable amount of force to replace /MDd (MultiThreadedDebugDLL) with /MD (MultiThreadedDLL)
+# and ensure that `_DEBUG` is not defined, as required by Unreal Engine's use of the release runtime library.
+# Only CMAKE_MSVC_RUNTIME_LIBRARY would be required if all our third-party libraries used CMake 3.15+, but alas.
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "")
+list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_MSVC_RUNTIME_LIBRARY:STRING=MultiThreadedDLL")
+list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CXX_FLAGS_DEBUG:STRING=/MD /Z7 /Ob0 /Od /RTC1")
+list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_C_FLAGS_DEBUG:STRING=/MD /Z7 /Ob0 /Od /RTC1")
 
 # When building official binaries on CI, use a very specific MSVC toolset version (which must be installed).
 # When building locally, use the default.
