e9b554d5f9304c74f610ab1f16205f710ca08ed6
Tim Moore <tim.moore@bentley.com>
2025-03-07 15:38:38 -0500

    Update to latest cesium-native and install its license

--- extern/vcpkg-overlays/cesium-native/portfile.cmake
+++ extern/vcpkg-overlays/cesium-native/portfile.cmake
@@ -12,8 +12,8 @@ endif()
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO CesiumGS/cesium-native
-        REF 9b8c823b8058d5265041782864721dbcccecf6c3
-        SHA512 ce28316ebf7e1423417c71b70d02457d292db7759ea33e01b37996d6294aeb14cf60d792b54e44c28724d59c8659bb2e72c1ec6cb562a43952c3e4a83dc95591
+        REF 70b8d69413b5fd8e5d8758ede46cc3be5e4f4f19
+        SHA512 46c94437c35964cd878fbae46f5b1890b33779b4afa9c30d9fbfe8f10f598bc68ed63ee820fb3f1eb7438499962e45db4a64b92b72c050f8c8e0285e66614b2e
         HEAD_REF vcpkg-pkg
         )
 
@@ -29,6 +29,7 @@ vcpkg_cmake_configure(
         )
 
 vcpkg_cmake_install()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cesium-native/cmake PACKAGE_NAME cesium-native)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
