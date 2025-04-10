130d59b59d22f18546e0923bc1b7070c2cf693d5
Tim Moore <tim.moore@cesium.com>
2025-04-10 15:27:38 +0200

    Android build and some CMake cleanup
    Start preparing for Cesium Native to not be a submodule anymore.

--- extern/vcpkg-overlays/cesium-native/portfile.cmake
+++ extern/vcpkg-overlays/cesium-native/portfile.cmake
@@ -12,8 +12,8 @@ endif()
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO CesiumGS/cesium-native
-        REF 70b8d69413b5fd8e5d8758ede46cc3be5e4f4f19
-        SHA512 46c94437c35964cd878fbae46f5b1890b33779b4afa9c30d9fbfe8f10f598bc68ed63ee820fb3f1eb7438499962e45db4a64b92b72c050f8c8e0285e66614b2e
+        REF 15a1ab097cddd3c58dab938011f9e6dae6f7405b
+        SHA512 d825b3a9ac70515084616a1871d783bd212d99d16ddaeb8beacbdc4dc10865cc6edc07f82061d4e7aa98e4ec840223fafa33844b6c4bd8cb992648aae4062eda
         HEAD_REF vcpkg-pkg
         )
 
