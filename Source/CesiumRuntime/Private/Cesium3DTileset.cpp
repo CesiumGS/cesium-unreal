14b1bb0574b211352e67d7cf19d578bbd6abe1a1
Ashley Rogers <ashley.rogers@bentley.com>
2025-05-01 11:15:22 -0400

    Fix build with latest native

--- Source/CesiumRuntime/Private/Cesium3DTileset.cpp
+++ Source/CesiumRuntime/Private/Cesium3DTileset.cpp
@@ -1228,7 +1228,10 @@ void ACesium3DTileset::LoadTileset() {
             this->ITwinID.IsEmpty() ? std::nullopt
                                     : std::make_optional<std::string>(
                                           TCHAR_TO_UTF8(*this->ITwinID)),
-            TCHAR_TO_UTF8(*this->ITwinAccessToken)),
+            TCHAR_TO_UTF8(*this->ITwinAccessToken),
+            [asyncSystem](const std::string&) {
+              return asyncSystem.createResolvedFuture<CesiumUtility::Result<std::string>>(CesiumUtility::Result<std::string>(std::string{}));
+            }),
         options);
     break;
   }
