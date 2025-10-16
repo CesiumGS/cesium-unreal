7e19b17b45264c95ab6c35a1442f0dd74ed6110b
Kevin Ring <kevin@kotachrome.com>
2025-10-16 16:57:08 +1100

    Doc tweaks.

--- Source/CesiumRuntime/Public/CesiumLoadedTile.h
+++ Source/CesiumRuntime/Public/CesiumLoadedTile.h
@@ -19,26 +19,43 @@ UINTERFACE()
 class UCesiumLoadedTile : public UInterface {
   GENERATED_BODY()
 };
-/** Access to the data loaded when a tile has been selected for display by the
- * tileset.
+
+/**
+ * Provides access to the details of a tile loaded by the @ref Cesium3DTileset.
+ * This interface is implemented by the `USceneComponent` subclass that
+ * represents the tile.
  */
 class ICesiumLoadedTile {
   GENERATED_BODY()
 public:
-  /** The tile identifier: this is informational only, as there is no
-   * guarantee of uniqueness */
+  /**
+   * Gets the tile identifier: this is informational only, as there is no
+   * guarantee of uniqueness.
+   */
   virtual const Cesium3DTilesSelection::TileID& GetTileID() const = 0;
-  /** The glTF model from which the tile data was obtained. */
+
+  /**
+   * Gets the glTF model from which the tile data was obtained.
+   */
   virtual const CesiumGltf::Model* GetGltfModel() const = 0;
-  /** The scaling factor to be applied (component-wise multiplication) to glTF
-   * vertices of this tile's model to obtain the values represented in their
-   * matching mesh component (see @{link
-   * UCesiumLoadedTilePrimitive::GetMeshComponent}). */
+
+  /**
+   * Gets the scaling factor that was applied (via component-wise
+   * multiplication) to the vertices of this tile's glTF model to obtain the
+   * values represented in the corresponding Unreal mesh components. See @ref
+   * ICesiumLoadedTilePrimitive::GetMeshComponent}.
+   */
   virtual FVector GetGltfToUnrealLocalVertexPositionScaleFactor() const = 0;
-  /** The tileset actor which the tile belongs to. */
+
+  /**
+   * Gets the tileset Actor that the tile belongs to.
+   */
   virtual ACesium3DTileset& GetTilesetActor() = 0;
-  /** The blueprint-accessible wrapper for metadata contained in the tile's glTF
-   * model. */
+
+  /**
+   * Gets the blueprint-accessible wrapper for metadata contained in the tile's
+   * glTF model.
+   */
   virtual const FCesiumModelMetadata& GetModelMetadata() const = 0;
 };
 
@@ -48,28 +65,49 @@ UINTERFACE()
 class UCesiumLoadedTilePrimitive : public UInterface {
   GENERATED_BODY()
 };
-/** Access to the data loaded for a given glTF primitive of an {@link
- * ICesiumLoadedTile}.
+
+/**
+ * Provides access to the details of a glTF `MeshPrimitive` loaded by the @ref
+ * Cesium3DTileset. This interface is implemented by the `USceneComponent`
+ * subclass that represents a single glTF primitive that is part of the tile's
+ * glTF model.
  */
 class ICesiumLoadedTilePrimitive {
   GENERATED_BODY()
 public:
-  /** The {@link ICesiumLoadedTile} the primitive belongs to. */
+  /**
+   * Gets the loaded tile that this primitive belongs to.
+   */
   virtual ICesiumLoadedTile& GetLoadedTile() = 0;
-  /** The Unreal mesh component built to represent the glTF primitive. */
+
+  /**
+   * Gets the Unreal static mesh component built to represent the glTF
+   * primitive.
+   */
   virtual UStaticMeshComponent& GetMeshComponent() = 0;
-  /** The Blueprint-accessible wrapper for the glTF Primitive's mesh features.
+
+  /**
+   * Gets the Blueprint-accessible wrapper for the glTF Primitive's mesh
+   * features.
    */
   virtual const FCesiumPrimitiveFeatures& GetPrimitiveFeatures() const = 0;
-  /** The Blueprint-accessible wrapper for the glTF Primitive's
-   * EXT_structural_metadata extension */
+
+  /**
+   * Gets the Blueprint-accessible wrapper for the glTF Primitive's
+   * `EXT_structural_metadata` extension.
+   */
   virtual const FCesiumPrimitiveMetadata& GetPrimitiveMetadata() const = 0;
-  /** The glTF primitive's geometry inside its {@link ICesiumLoadedTile}'s
-   * model. */
+
+  /**
+   * Gets the glTF primitive.
+   */
   virtual const CesiumGltf::MeshPrimitive* GetMeshPrimitive() const = 0;
-  /** Maps an accessor index in the glTF primitive to its corresponding texture
+
+  /**
+   * Maps an accessor index in the glTF primitive to its corresponding texture
    * coordinate index in the Unreal mesh. The -1 key is reserved for implicit
    * feature IDs (in other words, the vertex index).
+   *
    * @param AccessorIndex An accessor index in the glTF primitive
    * @return A texture coordinate index in the Unreal mesh, or std::nullopt if
    * none was found for the AccessorIndex passed.
