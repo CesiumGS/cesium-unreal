f8e618d0193185c7b5b098f20ca32355bbd7bb75
Tim Moore <tim.moore@cesium.com>
2026-02-16 15:58:22 +0100

    Deprecate CesiumCameraManger functions in favor of camera array

--- Source/CesiumRuntime/Public/CesiumCameraManager.h
+++ Source/CesiumRuntime/Public/CesiumCameraManager.h
@@ -69,42 +69,46 @@ public:
   ACesiumCameraManager();
 
   /**
-   * @brief Register a new camera with the camera manager.
-   *
-   * @param Camera The current state for the new camera.
-   * @return The generated ID for this camera. Use this ID to refer to the
-   * camera in the future when calling UpdateCamera.
+   * @brief DEPRECATED
+   * @deprecated The AdditionalCameras array should be used for directly storing
+   * and accessing cameras.
    */
+  UE_DEPRECATED(
+      "Cesium For Unreal v2.0",
+      "Use the AdditionalCameras array instead.")
   UFUNCTION(BlueprintCallable, Category = "Cesium")
   int32 AddCamera(UPARAM(ref) const FCesiumCamera& Camera);
 
   /**
-   * @brief Unregister an existing camera with the camera manager.
-   *
-   * @param CameraId The ID of the camera, as returned by AddCamera during
-   * registration.
-   * @return Whether the updating was successful. If false, the CameraId was
-   * invalid.
+   * @brief DEPRECATED
+   * @deprecated The AdditionalCameras array should be used for directly storing
+   * and accessing cameras.
    */
+  UE_DEPRECATED(
+      "Cesium For Unreal v2.0",
+      "Use the AdditionalCameras array instead.")
   UFUNCTION(BlueprintCallable, Category = "Cesium")
   bool RemoveCamera(int32 CameraId);
 
   /**
-   * @brief Update the state of the specified camera.
-   *
-   * @param CameraId The ID of the camera, as returned by AddCamera during
-   * registration.
-   * @param Camera The new, updated state of the camera.
-   * @return Whether the updating was successful. If false, the CameraId was
-   * invalid.
+   * @brief DEPRECATED
+   * @deprecated The AdditionalCameras array should be used for directly storing
+   * and accessing cameras.
    */
+  UE_DEPRECATED(
+      "Cesium For Unreal v2.0",
+      "Use the AdditionalCameras array instead.")
   UFUNCTION(BlueprintCallable, Category = "Cesium")
   bool UpdateCamera(int32 CameraId, UPARAM(ref) const FCesiumCamera& Camera);
 
   /**
-   * @brief Get a read-only map of the current camera IDs to cameras. These
-   * cameras have been added to the manager with {@link AddCamera}.
+   * @brief DEPRECATED
+   * @deprecated The AdditionalCameras array should be used for directly storing
+   * and accessing cameras.
    */
+  UE_DEPRECATED(
+      "Cesium For Unreal v2.0",
+      "Use the AdditionalCameras array instead.")
   UFUNCTION(BlueprintCallable, Category = "Cesium")
   const TMap<int32, FCesiumCamera>& GetCameras() const;
 
