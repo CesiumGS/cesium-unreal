4580c52703daea536d6e52ab72109d1a0321cc9a
Ashley Rogers <Ashley.Rogers@Bentley.com>
2026-06-25 16:23:39 -0400

    Take priority and 'essential' into account

--- .github/linux-prune-packages.js
+++ .github/linux-prune-packages.js
@@ -7,22 +7,32 @@ const packagesToKeep = [
   "core22",
   "pc-kernel",
   "pc",
-  "snapd"
+  "snapd",
+  "sudo"
 ];
 
 function runCommand(command) {
   return childProcess.execSync(command, { encoding: 'utf-8' });
 }
 
-const packagesList = runCommand("dpkg-query -Wf '${Package}\n'").trim().split("\n");
+const packagesList = runCommand("dpkg-query -Wf '${Package} ${Priority} ${Essential}\n'").trim().split("\n");
 const packagesObj = {};
-packagesList.forEach(pkg => {
-  packagesObj[pkg] = 'remove';
+packagesList.forEach(pkgInfo => {
+  const parts = pkgInfo.split(' ');
+  const pkg = parts[0];
+  const required = parts[1] == 'required';
+  const essential = parts[2] == 'yes';
+  if(required || essential) {
+    packagesObj[pkg] = 'unchecked';
+  }
+  else {
+    packagesObj[pkg] = 'remove';
+  }
 });
 
-function findPackageDependencies(packageName) {
+function findPackageDependencies(packages) {
   const regex = /(Depends: |Recommends: )([^\s]+)/g;
-  const result = runCommand(`apt depends ${packageName}`);
+  const result = runCommand(`apt depends ${packages.join(' ')}`);
   const matches = [...result.matchAll(regex)];
   matches.forEach(match => {
     const name = match[2].replace(/[\<\>]/g, '');
@@ -39,8 +49,8 @@ packagesToKeep.filter(pkg => packagesObj.hasOwnProperty(pkg)).forEach(pkg => {
 
 function runDependencyCheckIteration() {
   const uncheckedPackages = Object.keys(packagesObj).filter(pkg => packagesObj[pkg] === 'unchecked');
+  findPackageDependencies(uncheckedPackages);
   uncheckedPackages.forEach(pkg => {
-    findPackageDependencies(pkg);
     packagesObj[pkg] = 'keep';
   });
 }
